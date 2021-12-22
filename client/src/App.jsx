b39bc1ab3e4c2cd3e0d140aeea4e85e82136f4d6
conrad-evans <conradmugabe@gmail.com>
2021-12-22 14:51:24 +0300

    added routed pages to the entry file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,29 @@
+import { Box } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import SignUp from "./components/pages/SignUp";
+import LogIn from "./components/pages/LogIn";
+import RecoverPassword from "./components/pages/RecoverPassword";
+import RequireAuth from "./components/routing/RequireAuth";
+import Home from "./components/pages/Home";
+
 function App() {
-  return <h1>Fida Information Management System</h1>;
+  return (
+    <Box sx={{ height: "100vh", width: "100%" }}>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/recover-password" element={<RecoverPassword />} />
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
