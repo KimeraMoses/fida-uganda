04c63d64d61361c25ab95be6f8079e813232794e
conrad-evans <conradmugabe@gmail.com>
2021-12-22 14:34:12 +0300

    gave auth reducer a proper name

--- client/src/store/configStore.js
+++ client/src/store/configStore.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import api from "./middleware/api";
+import authReducer from "./reducers/auth";
 
 export default function configureAppStore() {
   const store = configureStore({
-    reducer: {},
+    reducer: { auth: authReducer },
 
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([api]),
   });
