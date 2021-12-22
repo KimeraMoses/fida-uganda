3a9919544e47abb706f9ffa4faa0258dea3af303
conrad-evans <conradmugabe@gmail.com>
2021-12-22 14:41:13 +0300

    added react router dom wrapper to application

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import configureAppStore from "./store/configStore";
 import App from "./App";
 import "./index.css";
@@ -8,10 +9,12 @@ import "./index.css";
 const store = configureAppStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
