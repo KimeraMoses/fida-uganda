930f06df19c560c34f73e203d6dc0e51178fc79d
conrad-evans <conradmugabe@gmail.com>
2021-12-22 14:33:37 +0300

    bug fixes && added headers to the payload

--- client/src/store/middleware/api.js
+++ client/src/store/middleware/api.js
@@ -9,29 +9,46 @@ const api =
       return next(action);
     }
 
-    next(action);
+    const {
+      url,
+      method,
+      data,
+      withCredentials,
+      headers,
+      onStart,
+      onSuccess,
+      onError,
+    } = action.payload;
 
-    const { url, method, data, withCredentials, onSuccess, onError } =
-      action.payload;
+    if (onStart) {
+      dispatch({ type: onStart });
+    }
+    next(action);
 
     try {
       const response = await axios({
+        baseURL: "https://fida-ims-staging.herokuapp.com",
         withCredentials,
         url,
         method,
         data,
+        headers: {
+          "Content-Type": "application/json",
+          ...headers,
+        },
       });
 
       dispatch(actions.apiCallSucceeded(response.data));
 
+      console.log(response.data);
       if (onSuccess) {
         dispatch({ type: onSuccess, payload: response.data });
       }
     } catch (error) {
-      dispatch(actions.apiCallFailed(error));
+      dispatch(actions.apiCallFailed(error.message));
 
       if (onError) {
-        dispatch({ type: onError, payload: error });
+        dispatch({ type: onError, payload: error.response.data });
       }
     }
   };
