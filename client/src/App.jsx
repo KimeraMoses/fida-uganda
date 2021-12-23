8366c2c5f2bb3b3bdeaaaddae5b3c3d8596a8148
conrad-evans <conradmugabe@gmail.com>
2021-12-23 14:06:58 +0300

    added logic fetch user data for valid tokens

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
-import { Box } from "@mui/material";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
 import SignUp from "./components/pages/SignUp";
 import LogIn from "./components/pages/LogIn";
 import RecoverPassword from "./components/pages/RecoverPassword";
 import RequireAuth from "./components/routing/RequireAuth";
 import Home from "./components/pages/Home";
+import { getUser } from "./store/reducers/auth";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUser());
+  });
+
   return (
     <Box sx={{ height: "100vh", width: "100%" }}>
       <Routes>
