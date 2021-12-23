89f3a003615a84ef71c6225e11d5a332ac2af951
conrad-evans <conradmugabe@gmail.com>
2021-12-23 14:07:21 +0300

    bug fixes

--- client/src/components/common/TopNav.jsx
+++ client/src/components/common/TopNav.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { Avatar, Stack, Typography } from "@mui/material";
 
 function TopNav() {
-  const { firstName } = useSelector((state) => state.auth.user);
+  const { first_name } = useSelector((state) => state.auth.user);
   return (
     <Stack
       direction="row"
@@ -10,7 +10,11 @@ function TopNav() {
       sx={{ px: "2rem", py: "0.5rem" }}
     >
       <Typography>
-        Hi <span color="purple">{firstName}</span>, Welcome back!
+        Hi{" "}
+        <span style={{ color: "purple", textTransform: "capitalize" }}>
+          {first_name}
+        </span>
+        , Welcome back!
       </Typography>
       <Stack direction="row" alignItems="center" sx={{ marginLeft: "auto" }}>
         <Avatar alt="first name" src="https://unsplash.random/100x100" />
