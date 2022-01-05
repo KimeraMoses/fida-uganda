7cb759a49e6f968569c0e1599f21098acfe4ae75
conrad-evans <conradmugabe@gmail.com>
2022-01-05 12:19:41 +0300

    added theme provider to app

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
 import configureAppStore from "./store/configStore";
 import App from "./App";
 import "./index.css";
+import theme from "./defaultData/theme";
 
 const store = configureAppStore();
 
@@ -12,7 +14,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
