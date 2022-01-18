2486829a18d09964d179d9f3b88ee0ff8e6e96c3
conrad-evans <conradmugabe@gmail.com>
2022-01-18 10:50:24 +0300

    added M&E department

---
+++ src/components/pages/m&e/FidaAssets.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import SectionHeader from "../../common/SectionHeader";
+
+function FidaAssets() {
+  return (
+    <Box>
+      <SectionHeader title="FIDA Assets" />
+    </Box>
+  );
+}
+
+export default FidaAssets;
