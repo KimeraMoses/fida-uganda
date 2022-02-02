b13c6e48c3e09a5b5b2694e3db900832150b7d65
conrad-evans <conradmugabe@gmail.com>
2022-02-02 13:06:41 +0300

    fixed log frame design

--- src/components/pages/programs/logframe/Header.jsx
+++ src/components/pages/programs/logframe/Header.jsx
@@ -1,127 +1,25 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 
-function Section({
-  title,
-  heading1,
-  heading2,
-  heading3,
-  heading4,
-  heading5,
-  heading6,
-  value1,
-  value2,
-  value3,
-  value4,
-  value5,
-  value6,
-  value7,
-  value8,
-  value9,
-  value10,
-  value11,
-  value12,
-  value13,
-}) {
+function Header() {
   return (
-    <Grid templateColumns="repeat(6, 1fr)" mb={"1rem"}>
-      <GridItem colSpan={5}>
-        <Grid
-          templateRows="repeat(4, 1fr)"
-          templateColumns="repeat(8, 1fr)"
-          mb={"1rem"}
-        >
-          <GridItem border="1px" px="0.5rem" as="div">
-            {title}
-          </GridItem>
-          <GridItem border="1px" px="0.5rem" as="div" bgColor="yellow.400">
-            {heading1}
-          </GridItem>
-          <GridItem
-            border="1px"
-            px="0.5rem"
-            as="div"
-            bgColor="yellow.400"
-          ></GridItem>
-          <GridItem border="1px" px="0.5rem" as="div" bgColor="green.400">
-            {heading2}
-          </GridItem>
-          <GridItem border="1px" px="0.5rem" as="div" bgColor="green.400">
-            {heading3}
-          </GridItem>
-          <GridItem border="1px" px="0.5rem" as="div" bgColor="green.400">
-            {heading4}
-          </GridItem>
-          <GridItem border="1px" px="0.5rem" as="div" bgColor="green.400">
-            {heading5}
-          </GridItem>
-          {heading6 ? (
-            <GridItem border="1px" px="0.5rem" as="div" bgColor="pink.200">
-              {heading6}
-            </GridItem>
-          ) : (
-            <GridItem border="1px" px="0.5rem" as="div"></GridItem>
-          )}
-          <GridItem border="1px" px="0.5rem" as="div" rowSpan={3}>
-            {value1}
-          </GridItem>
-          <GridItem border="1px" px="0.5rem" as="div" rowSpan={3}>
-            {value2}
-          </GridItem>
-          <GridItem border="1px" px="0.5rem" as="div">
-            Planned
-          </GridItem>
-          <GridItem border="1px" px="0.5rem" as="div">
-            {value3}
-          </GridItem>
-          <GridItem border="1px" px="0.5rem" as="div">
-            {value4}
-          </GridItem>
-          <GridItem border="1px" px="0.5rem" as="div">
-            {value5}
-          </GridItem>
-          <GridItem border="1px" px="0.5rem" as="div">
-            {value6}
-          </GridItem>
-          <GridItem border="1px" px="0.5rem" as="div">
-            {value7}
-          </GridItem>
-          <GridItem border="1px" px="0.5rem" as="div">
-            Achieved
-          </GridItem>
-          <GridItem border="1px" px="0.5rem" as="div" bgColor="purple.100">
-            {value8}
-          </GridItem>
-          <GridItem border="1px" px="0.5rem" as="div" bgColor="purple.100">
-            {value9}
-          </GridItem>
-          <GridItem border="1px" px="0.5rem" as="div">
-            {value10}
-          </GridItem>
-          <GridItem border="1px" px="0.5rem" as="div" bgColor="purple.100">
-            {value11}
-          </GridItem>
-          <GridItem border="1px" px="0.5rem" as="div" bgColor="purple.100">
-            {value12}
-          </GridItem>
-          <GridItem
-            border="1px"
-            px="0.5rem"
-            as="div"
-            colSpan={5}
-            textAlign="center"
-          >
-            Source
-          </GridItem>
-          <GridItem border="1px" px="0.5rem" as="div">
-            {value13}
-          </GridItem>
-        </Grid>
+    <Grid templateRows="repeat(2, 1fr)" templateColumns="repeat(6, 1fr)">
+      <GridItem border="1px" px="0.5rem" as="div" bgColor="yellow.400">
+        ORGANISATION NAME
       </GridItem>
-      <GridItem border="1px" px="0.5rem">
-        0%
+      <GridItem border="1px" px="0.5rem" as="div" colSpan={4}>
+        FIDA - UGANDA
+      </GridItem>
+      <GridItem border="1px" px="0.5rem" as="div" rowSpan={2} colSpan={1}>
+        Project Progress
+      </GridItem>
+      <GridItem border="1px" px="0.5rem" as="div" bgColor="yellow.400">
+        PROJECT NAME
+      </GridItem>
+      <GridItem border="1px" px="0.5rem" as="div" colSpan={4}>
+        SBVG NORTHERN
       </GridItem>
     </Grid>
   );
 }
 
-export default Section;
+export default Header;
