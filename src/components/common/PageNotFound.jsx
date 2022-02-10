fb0737993685282909a1122fc1601016d9fa74ed
conrad-evans <conradmugabe@gmail.com>
2022-02-10 12:00:15 +0300

    handled cases for navigation into unknown url during auth

--- src/components/common/PageNotFound.jsx
+++ src/components/common/PageNotFound.jsx
@@ -1,11 +1,28 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Heading, Text, VStack, Button } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
-function PageNotFound() {
+function NotFound() {
+  const navigate = useNavigate();
+
   return (
-    <Box height="100vh">
-      <Heading>Page Not Found</Heading>
-    </Box>
+    <VStack height="100vh" width="100%" gap="2rem">
+      <Heading fontSize="9xl" mt="10rem">
+        404
+      </Heading>
+      <VStack>
+        <Text fontSize="xl">Your find your self lost in an abyss</Text>
+        <Text fontSize="xl">Let's help you get home</Text>
+      </VStack>
+      <Button
+        onClick={() => navigate("/")}
+        bgColor="purple"
+        color="white"
+        _hover={{ bgColor: "purple.900" }}
+      >
+        Go Home
+      </Button>
+    </VStack>
   );
 }
 
-export default PageNotFound;
+export default NotFound;
