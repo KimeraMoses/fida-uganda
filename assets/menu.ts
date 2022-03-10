7316f5de868d642c58afff2818e73f028a10e179
conrad-evans <conradmugabe@gmail.com>
2022-03-10 15:37:46 +0300

    added app menu list

---
+++ assets/menu.ts
@@ -0,0 +1,7 @@
+import { MdSettings, MdTask, MdDashboard } from "react-icons/md"
+
+export const menuItems = [
+    { text: 'Dashboard', url: '/contact', icon: MdDashboard },
+    { text: 'Tasks', url: '/', icon: MdTask },
+    { text: 'Settings', url: '/about', icon: MdSettings },
+]
