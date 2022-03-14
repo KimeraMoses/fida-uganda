a4a30e5ae66f64854500d3d628f7eb5bbf71f345
conrad-evans <conradmugabe@gmail.com>
2022-03-14 09:52:19 +0300

    bug fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from "next";
-import {Text, Skeleton, Heading} from "@chakra-ui/react";
+import { Heading} from "@chakra-ui/react";
 import Layout from "../components/Layout";
 import Cards from "../components/Cards";
 import Card from "../components/Card";
-import usePosts from "../data/usePost";
+// import usePosts from "../data/usePost";
+import { MdSettings } from 'react-icons/md';
 
 const Home: NextPage = () => {
   // const { posts, isLoading } = usePosts();
@@ -11,15 +12,15 @@ const Home: NextPage = () => {
   return <Layout title="Dashboard">
     <Heading mt={10} mb={5} fontSize="2xl" color="purple.800" fontWeight="thin">Essentials</Heading>
     <Cards>
-      <Card />
-      <Card />
-      <Card />
+      <Card title="User logs" stat={5} icon={MdSettings} />
+      <Card title="User logs" stat={5} icon={MdSettings} />
+      <Card title="User logs" stat={5} icon={MdSettings} />
     </Cards>
     <Heading mt={10} mb={5} fontSize="2xl" color="purple.800" fontWeight="thin">Statistics</Heading>
     <Cards>
-      <Card />
-      <Card />
-      <Card />
+      <Card title="User logs" stat={5} icon={MdSettings} />
+      <Card title="User logs" stat={5} icon={MdSettings} />
+      <Card title="User logs" stat={5} icon={MdSettings} />
     </Cards>
   </Layout>;
 };
