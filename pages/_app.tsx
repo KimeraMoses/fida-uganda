78271379d4b90dee961310f85f34035231656d48
conrad-evans <conradmugabe@gmail.com>
2022-03-14 22:19:48 +0300

    configured swr for main app && added login page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { SWRConfig } from "swr";
 import { ChakraProvider } from "@chakra-ui/react";
+import { SWROptions } from "../data/swrProvider";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <SWRConfig value={SWROptions}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SWRConfig>
   );
 }
 
