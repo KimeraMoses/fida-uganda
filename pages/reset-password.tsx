ca6684e1c446673d182aeb98f58a2079e8feed21
conrad-evans <conradmugabe@gmail.com>
2022-03-15 13:50:06 +0300

    added password reset page

---
+++ pages/reset-password.tsx
@@ -0,0 +1,11 @@
+import PasswordResetForm from "../components/forms/PasswordResetForm";
+
+const ResetPassword = () => {
+  return (
+    <div>
+      <PasswordResetForm />
+    </div>
+  );
+};
+
+export default ResetPassword;
