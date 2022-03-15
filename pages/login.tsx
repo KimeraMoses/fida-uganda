3755196f0865047169b0753f2d8a50ce51041055
conrad-evans <conradmugabe@gmail.com>
2022-03-15 13:50:31 +0300

    bug fixes

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,12 +3,7 @@ import LoginForm from "../components/forms/LoginForm";
 
 const Login = () => {
   return (
-    <Box
-      h="100vh"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-    >
+    <Box h="100vh" display="flex" alignItems="center" justifyContent="center">
       <LoginForm />
     </Box>
   );
