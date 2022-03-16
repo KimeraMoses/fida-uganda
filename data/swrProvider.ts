ce47b76143390dfce9d47f0f8d37459cfdd3191f
conrad-evans <conradmugabe@gmail.com>
2022-03-17 01:04:27 +0300

    commented out swr provider code

--- data/swrProvider.ts
+++ data/swrProvider.ts
@@ -1,40 +1,41 @@
-import { SWRConfiguration } from "swr";
-import { NETWORK_ERROR_MSG } from "../assets/constants";
+export const prov = "Just leaving this here for now";
+// import { SWRConfiguration } from "swr";
+// import { NETWORK_ERROR_MSG } from "../assets/constants";
 
-declare global {
-  interface Error {
-    status: number;
-    info: any;
-  }
-}
+// declare global {
+//   interface Error {
+//     status: number;
+//     info: any;
+//   }
+// }
 
-const fetcher = async (input: RequestInfo, init?: RequestInit | undefined) => {
-  const response = await fetch(input, init);
+// const fetcher = async (input: RequestInfo, init?: RequestInit | undefined) => {
+//   const response = await fetch(input, init);
 
-  if (!response.ok) {
-    const error = new Error(NETWORK_ERROR_MSG);
+//   if (!response.ok) {
+//     const error = new Error(NETWORK_ERROR_MSG);
 
-    error.status = response.status;
-    error.info = await response.json();
+//     error.status = response.status;
+//     error.info = await response.json();
 
-    throw error;
-  }
+//     throw error;
+//   }
 
-  const data = await response.json();
-  return data;
-};
+//   const data = await response.json();
+//   return data;
+// };
 
-export const SWROptions: SWRConfiguration = {
-  refreshInterval: 3000, // refresh every 3 seconds
-  fetcher,
-  onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
-    if (retryCount >= 10) return;
-    setTimeout(() => revalidate({ retryCount }), 5000);
-  },
-  onError: (error, key) => {
-    if (error.status !== 403 && error.status !== 404) {
-      // Show a UI notification && send the error to sentry
-      console.log(error);
-    }
-  },
-};
+// export const SWROptions: SWRConfiguration = {
+//   refreshInterval: 3000, // refresh every 3 seconds
+//   fetcher,
+//   onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+//     if (retryCount >= 10) return;
+//     setTimeout(() => revalidate({ retryCount }), 5000);
+//   },
+//   onError: (error, key) => {
+//     if (error.status !== 403 && error.status !== 404) {
+//       // Show a UI notification && send the error to sentry
+//       console.log(error);
+//     }
+//   },
+// };
