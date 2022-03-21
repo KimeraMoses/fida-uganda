d145c07f154b952c090d30307acef5dea04feba1
conrad-evans <conradmugabe@gmail.com>
2022-03-21 17:11:42 +0300

    added login & auth router component

---
+++ src/components/compound/Auth.tsx
@@ -0,0 +1,31 @@
+import { Routes, Route, Outlet } from "react-router-dom";
+import AuthBgImage from "../common/AuthBgImage";
+import AuthFormBg from "../common/AuthFormBg";
+import PageNotFound from "../common/PageNotFound";
+// import PasswordReset from "./auth/PasswordReset";
+// import SignUp from "./auth/SignUp";
+import LogIn from './auth/Login';
+
+function Auth() {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <AuthBgImage>
+            <AuthFormBg>
+              <Outlet />
+            </AuthFormBg>
+          </AuthBgImage>
+        }
+      >
+        <Route index element={<LogIn />} />
+        {/* <Route path="/forgotpassword" element={<PasswordReset />} /> */}
+        {/* <Route path="/signup" element={<SignUp />} /> */}
+      </Route>
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  );
+}
+
+export default Auth;
---
+++ src/components/compound/auth/Login.tsx
@@ -0,0 +1,13 @@
+import FormHeader from "../../forms/auth/FormHeader";
+import LoginForm from "../../forms/auth/LoginForm";
+
+function LogIn() {
+  return (
+    <>
+      <FormHeader title="Sign In" />
+      <LoginForm />
+    </>
+  );
+}
+
+export default LogIn;
