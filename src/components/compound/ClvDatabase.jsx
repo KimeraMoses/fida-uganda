5ca388e7ee19b51eb5c6b267bc36fc02e0daf6ed
conrad-evans <conradmugabe@gmail.com>
2022-03-26 19:25:54 +0300

    added multi file select component

--- src/components/compound/ClvDatabase.jsx
+++ src/components/compound/ClvDatabase.jsx
@@ -1,9 +1,11 @@
 import SectionHeader from "../common/SectionHeader";
+import MultiUpload from "../common/MultiUpload";
 
 const ClvDatabase = () => {
   return (
     <>
       <SectionHeader title="Clv Database" />
+      <MultiUpload />
     </>
   );
 };
