b769b51768f53e146043a55f3647c2678ce7360c
Fortune <fortunemuhumuza@gmail.com>
2022-03-26 06:38:09 +0300

    added procurement table

---
+++ src/components/procurement/Procurements.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import SectionHeader from "../common/SectionHeader";
+import Table from "../common/Table";
+import { procurementsColumns } from "../../assets/tableColumns/procurements";
+import { useDisclosure } from "@chakra-ui/react";
+
+
+const Procurements = () => {
+  const [data, setdata] = useState([])
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const onRowClick = (row) => { };
+  return (
+    <>
+      <SectionHeader title="Fleet Database" />
+      <Table
+        data={data}
+        columns={procurementsColumns}
+        onRowClick={onRowClick}
+      />
+    </>
+  );
+};
+
+export default Procurements;
