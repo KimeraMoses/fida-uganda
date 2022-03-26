a4a945958d04db1e579fac3f99efb9912f1afd07
conrad-evans <conradmugabe@gmail.com>
2022-03-26 19:29:35 +0300

    bug fixes and minor changes

--- src/components/compound/ClvDatabase.jsx
+++ src/components/compound/ClvDatabase.jsx
@@ -1,11 +1,9 @@
 import SectionHeader from "../common/SectionHeader";
-import MultiUpload from "../common/MultiUpload";
 
 const ClvDatabase = () => {
   return (
     <>
       <SectionHeader title="Clv Database" />
-      <MultiUpload />
     </>
   );
 };
