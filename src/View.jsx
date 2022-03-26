1b17bd0535a17aab181d61ef1395c4f3d273b598
conrad-evans <conradmugabe@gmail.com>
2022-03-26 18:44:21 +0300

    added account settings

--- src/View.jsx
+++ src/View.jsx
@@ -8,6 +8,7 @@ import Tasks from "./components/compound/Tasks";
 import ClvDatabase from "./components/compound/ClvDatabase";
 import Reports from "./components/compound/Reports";
 import TravelOrder from "./components/compound/TravelOrder";
+import AccountSettings from "./components/compound/AccountSettings";
 
 const View = () => {
   return (
@@ -23,6 +24,7 @@ const View = () => {
         <Route index element={<Dashboard />} />
         <Route path="tasks" element={<Tasks />} />
         <Route path="clients" element={<Clients />} />
+        <Route path="account-settings" element={<AccountSettings />} />
         <Route path="case-files" element={<CaseFiles />} />
         <Route path="clv-database" element={<ClvDatabase />} />
         <Route path="requisitions" element={<Requisitions />} />
