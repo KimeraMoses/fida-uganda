ea6cd714da1c02b9a93b0901839d75516e98ae80
conrad-evans <conradmugabe@gmail.com>
2022-03-29 22:37:49 +0300

    commented out unused variables

--- src/components/itDepartment/ITProducts.jsx
+++ src/components/itDepartment/ITProducts.jsx
@@ -1,36 +1,36 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import SectionHeader from "../common/SectionHeader";
-import Table from "../common/Table";
-import { itProductsColumns } from "../../assets/tableColumns/itDepartment";
-import { useDisclosure } from "@chakra-ui/react";
-import Modal from "../common/Modal";
-import AddITProductForm from "../forms/it/AddITProductForm";
+// import Table from "../common/Table";
+// import { itProductsColumns } from "../../assets/tableColumns/itDepartment";
+// import { useDisclosure } from "@chakra-ui/react";
+// import Modal from "../common/Modal";
+// import AddITProductForm from "../forms/it/AddITProductForm";
 
 
 const ITProducts = () => {
-  const [data, setdata] = useState([])
+  // const [data, setData] = useState([])
 
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  // const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const onRowClick = (row) => { };
+  // const onRowClick = (row) => { };
   return (
     <>
       <SectionHeader title="IT Products" />
-      <Table
+      {/* <Table
         data={data}
         columns={itProductsColumns}
         onRowClick={onRowClick}
         btnLabel="Add Product"
         btnClick={onOpen}
-      />
-      <Modal isOpen={isOpen} onClose={onClose} title="IT Product Requisition Form">
-        <AddITProductForm
-        // onSubmit={mutate}
-        // isSubmitting={isSubmitting}
-        // isError={isError}
-        // error={error}
+      /> */}
+      {/* <Modal isOpen={isOpen} onClose={onClose} title="IT Product Requisition Form"> */}
+        {/* <AddITProductForm
+        onSubmit={mutate}
+        isSubmitting={isSubmitting}
+        isError={isError}
+        error={error}
         />
-      </Modal>
+      </Modal> */}
     </>
   );
 };
