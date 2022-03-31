7746850dd3e1f14e5fc3e86c8876a282fa838087
conrad-evans <conradmugabe@gmail.com>
2022-03-31 22:20:30 +0300

    added project files dynamic data

--- src/components/LegalAid/ProjectFiles/ProjectFiles.js
+++ src/components/LegalAid/ProjectFiles/ProjectFiles.js
@@ -1,16 +1,17 @@
 import React from "react";
+import { useProjects } from "../../../hooks/useProjects";
 import SectionHeader from "../../common/SectionHeader";
 import TableSearch from "../../common/table/TableSearch";
 import ProjectTable from "./ProjectPFilesTable/ProjectFilesTable";
 
 const ProjectFiles = () => {
+  const { data } = useProjects();
 
   return (
     <>
       <SectionHeader title="Project Files" />
-      <TableSearch showBtn={false}/>
-      <ProjectTable />
-
+      <TableSearch showBtn={false} />
+      {data && <ProjectTable data={data.projects} />}
     </>
   );
 };
