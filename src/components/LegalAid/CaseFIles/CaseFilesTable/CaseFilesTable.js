9c9c90a13810f16664c18e4318f6c9d195b4330a
conrad-evans <conradmugabe@gmail.com>
2022-04-05 16:58:11 +0300

    added case files

--- src/components/LegalAid/CaseFIles/CaseFilesTable/CaseFilesTable.js
+++ src/components/LegalAid/CaseFIles/CaseFilesTable/CaseFilesTable.js
@@ -3,243 +3,7 @@ import { Table, Thead, Tbody, Tr, Td } from "@chakra-ui/react";
 import classes from "./Table.module.css";
 import { TableHeadColumn } from "../../../Membership/Allocations/AllocationsTable/AllocationsTable";
 import { caseFilesColumns } from "../../../../assets/tableColumns/cases";
-
-export const FileData = [
-  {
-    fileNo: "FU/Kla/M/0001/2022 ",
-    date_Created: "01/01/2018",
-    village: "KISAASI - NAKAWA",
-    district: "KAMPALA",
-    nationality: "Ugandan",
-    nin: "CME90122167DME",
-    type: "Legal",
-    caseNo: "0002933",
-    gender: "Female",
-    name: "Cynthia Nakimera",
-    numBeneficiaries: 543,
-    occupation: "Dentist",
-    nature: "Family",
-    action: "Police Report",
-    nextVisit: "01/01/2018",
-    officer: "Missaga Clive",
-    referedBy: "Conrad",
-    reason: "Nothing",
-    feedback: "Nothing",
-    closingDate: "01/01/2018",
-    number: "08474638382",
-    disabilty: "none",
-    fida: "friend",
-    age: 23,
-  },
-  {
-    fileNo: "FU/Kla/M/0001/2022 ",
-    date_Created: "01/01/2018",
-    village: "KISAASI - NAKAWA",
-    district: "KAMPALA",
-    nationality: "Ugandan",
-    nin: "CME90122167DME",
-    type: "Legal",
-    caseNo: "0002933",
-    gender: "Female",
-    name: "Cynthia Nakimera",
-    numBeneficiaries: 543,
-    occupation: "Dentist",
-    nature: "Family",
-    action: "Police Report",
-    nextVisit: "01/01/2018",
-    officer: "Missaga Clive",
-    referedBy: "Conrad",
-    reason: "Nothing",
-    feedback: "Nothing",
-    closingDate: "01/01/2018",
-    number: "08474638382",
-    disabilty: "none",
-    fida: "friend",
-    age: 76,
-  },
-  {
-    fileNo: "FU/Kla/M/0001/2022 ",
-    date_Created: "01/01/2018",
-    village: "KISAASI - NAKAWA",
-    district: "KAMPALA",
-    nationality: "Ugandan",
-    nin: "CME90122167DME",
-    type: "Legal",
-    caseNo: "0002933",
-    gender: "Female",
-    name: "Cynthia Nakimera",
-    numBeneficiaries: 543,
-    occupation: "Dentist",
-    nature: "Family",
-    action: "Police Report",
-    nextVisit: "01/01/2018",
-    officer: "Missaga Clive",
-    referedBy: "Conrad",
-    reason: "Nothing",
-    feedback: "Nothing",
-    closingDate: "01/01/2018",
-    number: "08474638382",
-    disabilty: "none",
-    fida: "friend",
-    age: 76,
-  },
-  {
-    fileNo: "FU/Kla/M/0001/2022 ",
-    date_Created: "01/01/2018",
-    village: "KISAASI - NAKAWA",
-    district: "KAMPALA",
-    nationality: "Ugandan",
-    nin: "CME90122167DME",
-    type: "Legal",
-    caseNo: "0002933",
-    gender: "Female",
-    name: "Cynthia Nakimera",
-    numBeneficiaries: 543,
-    occupation: "Dentist",
-    nature: "Family",
-    action: "Police Report",
-    nextVisit: "01/01/2018",
-    officer: "Missaga Clive",
-    referedBy: "Conrad",
-    reason: "Nothing",
-    feedback: "Nothing",
-    closingDate: "01/01/2018",
-    number: "08474638382",
-    disabilty: "none",
-    fida: "friend",
-    age: 76,
-  },
-  {
-    fileNo: "FU/Kla/M/0001/2022 ",
-    date_Created: "01/01/2018",
-    village: "KISAASI - NAKAWA",
-    district: "KAMPALA",
-    nationality: "Ugandan",
-    nin: "CME90122167DME",
-    type: "Legal",
-    caseNo: "0002933",
-    gender: "Female",
-    name: "Cynthia Nakimera",
-    numBeneficiaries: 543,
-    occupation: "Dentist",
-    nature: "Family",
-    action: "Police Report",
-    nextVisit: "01/01/2018",
-    officer: "Missaga Clive",
-    referedBy: "Conrad",
-    reason: "Nothing",
-    feedback: "Nothing",
-    closingDate: "01/01/2018",
-    number: "08474638382",
-    disabilty: "none",
-    fida: "friend",
-    age: 76,
-  },
-  {
-    fileNo: "FU/Kla/M/0001/2022 ",
-    date_Created: "01/01/2018",
-    village: "KISAASI - NAKAWA",
-    district: "KAMPALA",
-    nationality: "Ugandan",
-    nin: "CME90122167DME",
-    type: "Legal",
-    caseNo: "0002933",
-    gender: "Female",
-    name: "Cynthia Nakimera",
-    numBeneficiaries: 543,
-    occupation: "Dentist",
-    nature: "Family",
-    action: "Police Report",
-    nextVisit: "01/01/2018",
-    officer: "Missaga Clive",
-    referedBy: "Conrad",
-    reason: "Nothing",
-    feedback: "Nothing",
-    closingDate: "01/01/2018",
-    number: "08474638382",
-    disabilty: "none",
-    fida: "friend",
-    age: 76,
-  },
-  {
-    fileNo: "FU/Kla/M/0001/2022 ",
-    date_Created: "01/01/2018",
-    village: "KISAASI - NAKAWA",
-    district: "KAMPALA",
-    nationality: "Ugandan",
-    nin: "CME90122167DME",
-    type: "Legal",
-    caseNo: "0002933",
-    gender: "Female",
-    name: "Cynthia Nakimera",
-    numBeneficiaries: 543,
-    occupation: "Dentist",
-    nature: "Family",
-    action: "Police Report",
-    nextVisit: "01/01/2018",
-    officer: "Missaga Clive",
-    referedBy: "Conrad",
-    reason: "Nothing",
-    feedback: "Nothing",
-    closingDate: "01/01/2018",
-    number: "08474638382",
-    disabilty: "none",
-    fida: "friend",
-    age: 76,
-  },
-  {
-    fileNo: "FU/Kla/M/0001/2022 ",
-    date_Created: "01/01/2018",
-    village: "KISAASI - NAKAWA",
-    district: "KAMPALA",
-    nationality: "Ugandan",
-    nin: "CME90122167DME",
-    type: "Legal",
-    caseNo: "0002933",
-    gender: "Female",
-    name: "Cynthia Nakimera",
-    numBeneficiaries: 543,
-    occupation: "Dentist",
-    nature: "Family",
-    action: "Police Report",
-    nextVisit: "01/01/2018",
-    officer: "Missaga Clive",
-    referedBy: "Conrad",
-    reason: "Nothing",
-    feedback: "Nothing",
-    closingDate: "01/01/2018",
-    number: "08474638382",
-    disabilty: "none",
-    fida: "friend",
-    age: 76,
-  },
-  {
-    fileNo: "FU/Kla/M/0001/2022 ",
-    date_Created: "01/01/2018",
-    village: "KISAASI - NAKAWA",
-    district: "KAMPALA",
-    nationality: "Ugandan",
-    nin: "CME90122167DME",
-    type: "Legal",
-    caseNo: "0002933",
-    gender: "Female",
-    name: "Cynthia Nakimera",
-    numBeneficiaries: 543,
-    occupation: "Dentist",
-    nature: "Family",
-    action: "Police Report",
-    nextVisit: "01/01/2018",
-    officer: "Missaga Clive",
-    referedBy: "Conrad",
-    reason: "Nothing",
-    feedback: "Nothing",
-    closingDate: "01/01/2018",
-    number: "08474638382",
-    disabilty: "none",
-    fida: "friend",
-    age: 76,
-  },
-];
+import { formatDate } from "../../../../lib/data";
 
 const CaseFilesTable = ({ data }) => {
   return (
@@ -267,37 +31,37 @@ const CaseFilesTable = ({ data }) => {
             </Tr>
           </Thead>
           <Tbody>
-            {FileData.map((item) => {
+            {data.map((item) => {
               return (
-                <Tr key={item.age}>
-                  <Td className={classes.primary_text_icon}>{item.fileNo}</Td>
+                <Tr key={item.id}>
+                  <Td className={classes.primary_text_icon}>{item.id}</Td>
                   <Td className={classes.data__purpose_primary_text}>
-                    {item.date_Created}
+                    {formatDate(item.createdAt)}
                   </Td>
-                  <Td>{item.village}</Td>
+                  <Td>{item.complainant.village}</Td>
                   <Td>{item.district}</Td>
-                  <Td>{item.nationality}</Td>
-                  <Td>{item.nin}</Td>
+                  <Td>{item.complainant.country}</Td>
+                  <Td>{item.complainant.nin}</Td>
                   <Td>{item.type}</Td>
-                  <Td>{item.date_Created}</Td>
-                  <Td>{item.caseNo}</Td>
-                  <Td>{item.gender}</Td>
-                  <Td>{item.name}</Td>
-                  <Td>{item.age}</Td>
-                  <Td>{item.numBeneficiaries}</Td>
-                  <Td>{item.occupation}</Td>
+                  <Td>{formatDate(item.createdAt)}</Td>
+                  <Td>{item.id}</Td>
+                  <Td>{item.complainant.sex}</Td>
+                  <Td>{item.complainant.name}</Td>
+                  <Td>{item.complainant.age}</Td>
+                  <Td>{item.complainant.numBeneficiaries}</Td>
+                  <Td>{item.complainant.occupation}</Td>
                   <Td>{item.nature}</Td>
                   <Td>{item.action}</Td>
                   <Td>{item.nextVisit}</Td>
                   <Td>{item.officer}</Td>
-                  <Td>{item.referedBy}</Td>
+                  <Td>{item.referredBy}</Td>
                   <Td>{item.reason}</Td>
                   <Td>{item.feedback}</Td>
-                  <Td>{item.date_Created}</Td>
-                  <Td>{item.number}</Td>
-                  <Td>{item.disabilty}</Td>
-                  <Td>{item.name}</Td>
-                  <Td>{item.number}</Td>
+                  <Td>{formatDate(item.createdAt)}</Td>
+                  <Td>{item.complainant.phoneNumber}</Td>
+                  <Td>{item.complainant.disability}</Td>
+                  <Td>{item.respondentName}</Td>
+                  <Td>{item.respondentPhone}</Td>
                   <Td>{item.fida}</Td>
                 </Tr>
               );
