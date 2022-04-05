052728159ee8eedc50db34963e0ea99817f237f6
conrad-evans <conradmugabe@gmail.com>
2022-04-06 01:35:13 +0300

    added report form hooks

--- src/components/HumanResource/Reports/NewReportForm/NewReportForm.js
+++ src/components/HumanResource/Reports/NewReportForm/NewReportForm.js
@@ -1,62 +1,99 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import classes from "../../../Membership/MembersActivities/NewActivityForm/NewActivityForm.module.css";
 import InputField from "../../../Membership/MembersActivities/NewActivityForm/InputField";
 import FormButton from "../../../Membership/MembersActivities/NewActivityForm/Button/FormButton";
+import { Form, Formik } from "formik";
+import { useToast } from "@chakra-ui/react";
+import { toastError } from "../../../../lib/toastDetails";
 
-const NewReportForm = (props) => {
-  const { onClose } = props;
+const NewReportForm = ({ onClose, error, isError, onSubmit, isSubmitting }) => {
+  const [file, setFile] = useState(null);
+  const toast = useToast();
+
+  useEffect(() => {
+    if (isError) {
+      toast(toastError(error));
+    }
+  }, [isError, error, toast]);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setFile(file);
+  };
+
   return (
-    <div className={classes.activity_form_wrapper} style={{padding: '10px 20px'}}>
-      <form>
-        <div className={classes.input_group_wrapper}>
-          <div className={classes.input_label}>Report Title</div>
-          <div className={classes.input_field_wrapper}>
-            <InputField placeholder="Type here" fullWidth name="" />
+    <Formik
+      onSubmit={(values) => {
+        const formData = new FormData();
+        formData.append("report", file);
+        Object.keys(values).forEach((key) => {
+          formData.append(key, values[key]);
+        });
+        onSubmit(formData);
+      }}
+    >
+      <div
+        className={classes.activity_form_wrapper}
+        style={{ padding: "10px 20px" }}
+      >
+        <Form>
+          <div className={classes.input_group_wrapper}>
+            <div className={classes.input_label}>Report Title</div>
+            <div className={classes.input_field_wrapper}>
+              <InputField placeholder="Type here" fullWidth name="title" />
+            </div>
           </div>
-        </div>
-        <div className={classes.input_group_wrapper}>
-          <div className={classes.input_label}>Supervisor’s Name</div>
-          <div className={classes.input_field_wrapper}>
-            <InputField placeholder="Type here" fullWidth name="" />
+          <div className={classes.input_group_wrapper}>
+            <div className={classes.input_label}>Supervisor’s Name</div>
+            <div className={classes.input_field_wrapper}>
+              <InputField
+                placeholder="Type here"
+                fullWidth
+                name="supervisor_name"
+              />
+            </div>
           </div>
-        </div>
-        <div className={classes.input_group_wrapper}>
-          <div className={classes.input_label}>Reporting Period</div>
-          <div className={classes.input_field_wrapper}>
-            <InputField placeholder="Type here" fullWidth name="" />
+          <div className={classes.input_group_wrapper}>
+            <div className={classes.input_label}>Reporting Period</div>
+            <div className={classes.input_field_wrapper}>
+              <InputField placeholder="Type here" fullWidth name="period" />
+            </div>
           </div>
-        </div>
-        <div className={classes.input_group_wrapper}>
-          <div className={classes.input_label}>Date</div>
-          <div className={classes.input_field_wrapper}>
-            <InputField type="date" placeholder="Type here" fullWidth name="" />
+          <div className={classes.input_group_wrapper}>
+            <div className={classes.input_label}>Date</div>
+            <div className={classes.input_field_wrapper}>
+              <InputField
+                type="date"
+                placeholder="Type here"
+                fullWidth
+                name="date"
+              />
+            </div>
           </div>
-        </div>
-        <div className={classes.input_group_wrapper}>
-          <div className={classes.input_label}>
-            <FormButton variant="colored">Upload File</FormButton>
+          <div className={classes.input_group_wrapper}>
+            <div className={classes.input_label}>
+              <FormButton variant="colored">Upload File</FormButton>
+            </div>
+            <div className={classes.input_field_wrapper}>
+              <input type="file" onClick={handleFileChange} />
+            </div>
           </div>
-          <div className={classes.input_field_wrapper}>
-            <InputField
-              placeholder="Click to browse or drag and drop your files"
-              disabled={true}
-              fullWidth
-              name=""
-              type="file"
-            />
-          </div>
-        </div>
 
-        <div className={classes.form_action_wrapper}>
-          <FormButton variant="cancel" type="button" onClick={() => onClose()}>
-            cancel
-          </FormButton>
-          <FormButton variant="save" type="submit">
-            Save and Exit
-          </FormButton>
-        </div>
-      </form>
-    </div>
+          <div className={classes.form_action_wrapper}>
+            <FormButton
+              variant="cancel"
+              type="button"
+              onClick={() => onClose()}
+            >
+              cancel
+            </FormButton>
+            <FormButton variant="save" type="submit">
+              Save and Exit
+            </FormButton>
+          </div>
+        </Form>
+      </div>
+    </Formik>
   );
 };
 
--- src/components/HumanResource/Reports/Reports.js
+++ src/components/HumanResource/Reports/Reports.js
@@ -5,11 +5,12 @@ import SectionHeader from "../../common/SectionHeader";
 import TableSearch from "../../common/table/TableSearch";
 import NewReportForm from "./NewReportForm/NewReportForm";
 import ReportsTable from "./ReportTable/ReportTable";
-import { useReports } from "../../../hooks/useReports";
+import { useAddReport, useReports } from "../../../hooks/useReports";
 
 const Reports = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { data } = useReports();
+  const { mutate, isLoading, isError, error } = useAddReport();
 
   return (
     <>
@@ -17,7 +18,13 @@ const Reports = () => {
       <TableSearch btnLabel="Add Report" btnClick={onOpen} />
       {data && <ReportsTable data={data?.reports} />}
       <Modal isOpen={isOpen} onClose={onClose}>
-        <NewReportForm onClose={onClose} />
+        <NewReportForm
+          onClose={onClose}
+          onSubmit={mutate}
+          isSubmitting={isLoading}
+          isError={isError}
+          error={error}
+        />
       </Modal>
     </>
   );
