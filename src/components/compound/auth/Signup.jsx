8f500a530c18aba7c8a5012268e8abb70ceb930d
conrad-evans <conradmugabe@gmail.com>
2022-04-08 00:00:49 +0300

    added sign up hook to application

--- src/components/compound/auth/Signup.jsx
+++ src/components/compound/auth/Signup.jsx
@@ -1,11 +1,33 @@
 import FormHeader from "../../forms/auth/FormHeader";
 import SignUpForm from "../../forms/auth/SignUpForm";
+import { useSignUp } from "../../../hooks/useUser";
+import { useToast } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { toastError } from "../../../lib/toastDetails";
 
 function SignUp() {
+  const { mutate, isLoading, isSuccess, isError, error } = useSignUp();
+  const toast = useToast();
+
+  useEffect(() => {
+    if (isSuccess) {
+      toast(
+        toastError(
+          "You have successfully created an account. Contact Human Resources for activation"
+        )
+      );
+    }
+  }, [toast, isSuccess]);
+
   return (
     <>
       <FormHeader title="Sign Up" />
-      <SignUpForm />
+      <SignUpForm
+        onSubmit={mutate}
+        isSubmitting={isLoading}
+        isError={isError}
+        error={error}
+      />
     </>
   );
 }
