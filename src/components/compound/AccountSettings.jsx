72a0dd3a0c9cf5d28137437e505cbd469b6ce2de
conrad-evans <conradmugabe@gmail.com>
2022-04-20 16:26:49 +0300

    bug fixes

--- src/components/compound/AccountSettings.jsx
+++ src/components/compound/AccountSettings.jsx
@@ -5,17 +5,20 @@ import { useToast } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import { useUpdateProfile } from "../../hooks/useUser";
 import { toastSuccess } from "../../lib/toastDetails";
+import { useNavigate } from "react-router-dom";
 
 const AccountSettings = () => {
   const { user } = useSelector((state) => state.auth);
   const { mutate, isLoading, isSuccess, isError, error } = useUpdateProfile();
   const toast = useToast();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (isSuccess) {
       toast(toastSuccess("Profile updated successfully"));
+      navigate("/");
     }
-  }, [isSuccess, toast]);
+  }, [isSuccess, toast, navigate]);
 
   return (
     <>
