2b782d30698093494b3e2c9e5a9131d710af9a59
conrad-evans <conradmugabe@gmail.com>
2022-04-27 00:57:18 +0300

    fixed missing fleet menu option causing app crash

--- src/components/fleetManager/FleetDatabase.jsx
+++ src/components/fleetManager/FleetDatabase.jsx
@@ -1,4 +1,3 @@
-// import { useState } from 'react'
 import SectionHeader from "../common/SectionHeader";
 import { useToast } from "@chakra-ui/react";
 import { useEffect } from "react";
@@ -10,23 +9,19 @@ import FleetDatabaseForm from "../forms/fleetDatabase/FleetDatabaseForm";
 import { useDisclosure } from "@chakra-ui/react";
 import { useFleets } from "../../hooks/useFleet";
 
-
 const FleetDatabase = () => {
-  // const [data, setdata] = useState([])
-
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const { data, isLoading, isError, error } = useFleets();
   const toast = useToast();
 
   useEffect(() => {
-    
     if (isError) {
       toast(toastError(error));
     }
   }, [isError, error, toast]);
 
-  const onRowClick = (row) => { };
+  const onRowClick = (row) => {};
   return (
     <>
       <SectionHeader title="Fleet Database" />
@@ -40,10 +35,10 @@ const FleetDatabase = () => {
       />
       <Modal isOpen={isOpen} onClose={onClose} title="Vehicle Profiling Form">
         <FleetDatabaseForm
-          // onSubmit={mutate}
-          // isSubmitting={isSubmitting}
-          // isError={isError}
-          // error={error}
+        // onSubmit={mutate}
+        // isSubmitting={isSubmitting}
+        // isError={isError}
+        // error={error}
         />
       </Modal>
     </>
