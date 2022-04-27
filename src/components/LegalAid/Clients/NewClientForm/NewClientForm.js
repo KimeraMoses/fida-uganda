96f7feb120712303b651683d2ffe7d70f4dcc1aa
conrad-evans <conradmugabe@gmail.com>
2022-04-27 17:02:31 +0300

    switched Client form to use `withForm` hoc && added logic for adding a client

--- src/components/LegalAid/Clients/NewClientForm/NewClientForm.js
+++ src/components/LegalAid/Clients/NewClientForm/NewClientForm.js
@@ -2,271 +2,136 @@ import React from "react";
 import classes from "../../../Membership/Members/NewMemberForm/MultiForm/MultiForm.module.css";
 import styles from "../../../Membership/Members/NewMemberForm/MultiForm/MultiForm.module.css";
 import { SimpleGrid } from "@chakra-ui/react";
-import { Form, Formik } from "formik";
 import SelectField from "../../../common/SelectField";
 import InputField from "../../../common/UI/InputField/InputField";
 import FormButton from "../../../common/UI/FormButton/FormButton";
-const NewClientForm = (props) => {
-  const { isSubmitting } = props;
+import withForm from "../../../../hoc/withForm";
+
+const NewClientForm = ({ isSubmitting }) => {
   return (
-    <Formik
-      //   initialValues={initialValues}
-      //   validationSchema={caseFileSchema}
-      onSubmit={(values) => {
-        console.log("Some thing");
-      }}
-    >
-      {({ values }) => {
-        return (
-          <div className={classes.form_wrapper}>
-            <Form>
-              <div className={classes.field_wrapper}>
-                <SimpleGrid
-                  columns={2}
-                  spacing={1}
-                  style={{ alignItems: "center" }}
-                >
-                  <div className={styles.field_row_label}>Name</div>
-                  <InputField
-                    placeholder="Type Here"
-                    name="complainantName"
-                    fullwidth
-                  />
-                </SimpleGrid>
-                <SimpleGrid
-                  columns={2}
-                  spacing={1}
-                  style={{ alignItems: "center" }}
-                >
-                  <div className={styles.field_row_label}>Sex</div>
-                  <SelectField
-                    name="complainantSex"
-                    options={[
-                      { label: "Male", value: "M" },
-                      { label: "Female", value: "F" },
-                    ]}
-                  />
-                </SimpleGrid>
-                <SimpleGrid
-                  columns={2}
-                  spacing={1}
-                  style={{ alignItems: "center" }}
-                >
-                  <div className={styles.field_row_label}>Age</div>
-                  <InputField
-                    placeholder="Type Here"
-                    name="complainantAge"
-                    type="number"
-                    fullwidth
-                  />
-                </SimpleGrid>
-                <SimpleGrid
-                  columns={2}
-                  spacing={1}
-                  style={{ alignItems: "center" }}
-                >
-                  <div className={styles.field_row_label}>Telephone Number</div>
-                  <InputField
-                    fullwidth
-                    placeholder="Type Here"
-                    name="complainantPhoneNumber"
-                  />
-                </SimpleGrid>
-                <SimpleGrid
-                  columns={2}
-                  spacing={1}
-                  style={{ alignItems: "center" }}
-                >
-                  <div className={styles.field_row_label}>Country</div>
-                  <InputField
-                    placeholder="Type Here"
-                    name="complainantCountry"
-                    type="number"
-                    fullwidth
-                  />
-                </SimpleGrid>
-                <SimpleGrid
-                  columns={2}
-                  spacing={1}
-                  style={{ alignItems: "center" }}
-                >
-                  <div className={styles.field_row_label}>
-                    NATIONAL IDENTIFICATION No.
-                  </div>
-                  <InputField
-                    placeholder="Type Here"
-                    name="complainantNIN"
-                    fullwidth
-                  />
-                </SimpleGrid>
-                <SimpleGrid
-                  columns={2}
-                  spacing={1}
-                  style={{ alignItems: "center" }}
-                >
-                  <div className={styles.field_row_label}>District</div>
-                  <InputField
-                    placeholder="Type Here"
-                    name="complainantDistrict"
-                    fullwidth
-                  />
-                </SimpleGrid>
-                <SimpleGrid
-                  columns={2}
-                  spacing={1}
-                  style={{ alignItems: "center" }}
-                >
-                  <div className={styles.field_row_label}>
-                    SubCounty/Town Council
-                  </div>
-                  <InputField
-                    placeholder="Type Here"
-                    name="complainantSubCounty"
-                    fullwidth
-                  />
-                </SimpleGrid>
-                <SimpleGrid
-                  columns={2}
-                  spacing={1}
-                  style={{ alignItems: "center" }}
-                >
-                  <div className={styles.field_row_label}>Parish</div>
-                  <InputField
-                    placeholder="Type Here"
-                    name="complainantParish"
-                    fullwidth
-                  />
-                </SimpleGrid>
-                <SimpleGrid
-                  columns={2}
-                  spacing={1}
-                  style={{ alignItems: "center" }}
-                >
-                  <div className={styles.field_row_label}>Village</div>
-                  <InputField
-                    placeholder="Type Here"
-                    name="complainantVillage"
-                    fullwidth
-                  />
-                </SimpleGrid>
-                <SimpleGrid
-                  columns={2}
-                  spacing={1}
-                  style={{ alignItems: "center" }}
-                >
-                  <div className={styles.field_row_label}>Parish</div>
-                  <InputField
-                    placeholder="Type Here"
-                    name="complainantParish"
-                    fullwidth
-                  />
-                </SimpleGrid>
-                <SimpleGrid
-                  columns={2}
-                  spacing={1}
-                  style={{ alignItems: "center" }}
-                >
-                  <div className={styles.field_row_label}>Marital Status</div>
-                  <InputField
-                    placeholder="Type Here"
-                    name="complainantMaritalStatus"
-                    fullwidth
-                  />
-                </SimpleGrid>
-                <SimpleGrid
-                  columns={2}
-                  spacing={1}
-                  style={{ alignItems: "center" }}
-                >
-                  <div className={styles.field_row_label}>Accompanied By</div>
-                  <InputField
-                    placeholder="Type Here"
-                    name="complainantAccompaniedBy"
-                    fullwidth
-                  />
-                </SimpleGrid>
-                <SimpleGrid
-                  columns={2}
-                  spacing={1}
-                  style={{ alignItems: "center" }}
-                >
-                  <div className={styles.field_row_label}>Job</div>
-                  <InputField
-                    placeholder="Type Here"
-                    name="complainantJob"
-                    fullwidth
-                  />
-                </SimpleGrid>
-                <SimpleGrid
-                  columns={2}
-                  spacing={1}
-                  style={{ alignItems: "center" }}
-                >
-                  <div className={styles.field_row_label}>Place of work</div>
-                  <InputField
-                    placeholder="Type Here"
-                    name="complainantPlaceOfWork"
-                    fullwidth
-                  />
-                </SimpleGrid>
-                <SimpleGrid
-                  columns={2}
-                  spacing={1}
-                  style={{ alignItems: "center" }}
-                >
-                  <div className={styles.field_row_label}>
-                    Level of Education
-                  </div>
-                  <InputField
-                    placeholder="Type Here"
-                    name="complainantLevelOfEducation"
-                    fullwidth
-                  />
-                </SimpleGrid>
-                <SimpleGrid
-                  columns={2}
-                  spacing={1}
-                  style={{ alignItems: "center" }}
-                >
-                  <div className={styles.field_row_label}>
-                    Preferred Language
-                  </div>
-                  <InputField
-                    placeholder="Type Here"
-                    name="complainantPreferredLanguage"
-                    fullwidth
-                  />
-                </SimpleGrid>
-                <SimpleGrid
-                  columns={2}
-                  spacing={1}
-                  style={{ alignItems: "center" }}
-                >
-                  <div className={styles.field_row_label}>
-                    Relationship with Respondent
-                  </div>
-                  <InputField
-                    placeholder="Type Here"
-                    name="complainantRelationshipWithRespondent"
-                    fullwidth
-                  />
-                </SimpleGrid>
-              </div>
-              <div style={{ float: "right", padding: "20px 0" }}>
-                <FormButton
-                  variant="colored"
-                  rounded={true}
-                  disabled={isSubmitting}
-                >
-                  {isSubmitting ? "Adding..." : "Add Client"}
-                </FormButton>
-              </div>
-            </Form>
+    <div className={classes.form_wrapper}>
+      <div className={classes.field_wrapper}>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>Name</div>
+          <InputField placeholder="Type Here" name="name" fullwidth />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>Sex</div>
+          <SelectField
+            name="sex"
+            placeholder="Select Gender"
+            options={[
+              { label: "Male", value: "Male" },
+              { label: "Female", value: "Female" },
+            ]}
+          />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>Email</div>
+          <InputField
+            placeholder="Type Here"
+            name="email"
+            fullwidth
+            type="email"
+          />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>Age</div>
+          <InputField
+            placeholder="Type Here"
+            name="age"
+            fullwidth
+            maxLength={3}
+          />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>Telephone Number</div>
+          <InputField
+            fullwidth
+            placeholder="Type Here"
+            name="phoneNumber"
+            maxLength={12}
+          />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>Address</div>
+          <InputField placeholder="Type Here" name="address" fullwidth />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>Country</div>
+          <InputField placeholder="Type Here" name="country" fullwidth />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>
+            NATIONAL IDENTIFICATION No.
           </div>
-        );
-      }}
-    </Formik>
+          <InputField placeholder="Type Here" name="NIN" fullwidth />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>District</div>
+          <InputField placeholder="Type Here" name="district" fullwidth />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>SubCounty/Town Council</div>
+          <InputField placeholder="Type Here" name="subcounty" fullwidth />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>Parish</div>
+          <InputField placeholder="Type Here" name="parish" fullwidth />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>Village</div>
+          <InputField placeholder="Type Here" name="village" fullwidth />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>Marital Status</div>
+          <InputField placeholder="Type Here" name="marital_status" fullwidth />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>Accompanied By</div>
+          <InputField placeholder="Type Here" name="accompaniedBy" fullwidth />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>Job</div>
+          <InputField placeholder="Type Here" name="occupation" fullwidth />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>Place of work</div>
+          <InputField placeholder="Type Here" name="place_of_work" fullwidth />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>Level of Education</div>
+          <InputField
+            placeholder="Type Here"
+            name="level_of_education"
+            fullwidth
+          />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>Preferred Language</div>
+          <InputField
+            placeholder="Type Here"
+            name="preferred_language"
+            fullwidth
+          />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={1} style={{ alignItems: "center" }}>
+          <div className={styles.field_row_label}>Number of Beneficiaries</div>
+          <InputField
+            placeholder="Type Here"
+            name="num_beneficiaries"
+            fullwidth
+            maxLength={2}
+          />
+        </SimpleGrid>
+      </div>
+      <div style={{ float: "right", padding: "20px 0" }}>
+        <FormButton variant="colored" rounded={true} disabled={isSubmitting}>
+          {isSubmitting ? "Adding..." : "Add Client"}
+        </FormButton>
+      </div>
+    </div>
   );
 };
 
-export default NewClientForm;
+export default withForm(NewClientForm);
