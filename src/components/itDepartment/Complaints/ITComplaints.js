3581019fee0a9488626cb93c198735aa3e2f6897
Fortune <fortunemuhumuza@gmail.com>
2022-05-01 23:18:01 +0300

    implemented fetch IT complaints

--- src/components/itDepartment/Complaints/ITComplaints.js
+++ src/components/itDepartment/Complaints/ITComplaints.js
@@ -4,14 +4,18 @@ import Modal from "../../common/Modal";
 import SectionHeader from "../../common/SectionHeader";
 import TableSearch from "../../common/table/TableSearch";
 import ComplaintsTable from "./ComplaintTable/ComplaintTable";
+import {useComplaints} from '../../../hooks/useComplaint'
 
 const ITComplaints = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { data, isLoading } = useComplaints();
   return (
     <>
       <SectionHeader title="IT Complaints" />
       <TableSearch btnLabel="Add Complaint" btnClick={onOpen} />
-      <ComplaintsTable />
+      <ComplaintsTable
+      data={data?.complaints}
+      />
       <Modal isOpen={isOpen} onClose={onClose} title="New Complaint Form">
         Complaint Form
       </Modal>
