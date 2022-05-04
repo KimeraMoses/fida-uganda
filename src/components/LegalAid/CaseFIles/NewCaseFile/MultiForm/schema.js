a892b5e4e4faffb5f876c70fae605830bfb1f1c6
conrad-evans <conradmugabe@gmail.com>
2022-05-04 14:10:43 +0300

    WIP:: adding case file

--- src/components/LegalAid/CaseFIles/NewCaseFile/MultiForm/schema.js
+++ src/components/LegalAid/CaseFIles/NewCaseFile/MultiForm/schema.js
@@ -1,23 +1,6 @@
 import * as Yup from "yup";
 
 export const caseFileInitialValues = {
-  complainantName: "",
-  complainantSex: "",
-  complainantAge: "",
-  complainantPhoneNumber: "",
-  complainantCountry: "",
-  complainantNIN: "",
-  complainantDistrict: "",
-  complainantSubCounty: "",
-  complainantParish: "",
-  complainantVillage: "",
-  complainantMaritalStatus: "",
-  complainantAccompaniedBy: "",
-  complainantJob: "",
-  complainantPlaceOfWork: "",
-  complainantLevelOfEducation: "",
-  complainantPreferredLanguage: "",
-  complainantRelationshipWithRespondent: "",
   respondentName: "",
   respondentSex: "",
   respondentAge: "",
@@ -34,7 +17,10 @@ export const caseFileInitialValues = {
   respondentPlaceOfWork: "",
   respondentLevelOfEducation: "",
   respondentPreferredLanguage: "",
-  respondentRelationshipWithRespondent: "",
+  respondentRelationshipWithComplainant: "",
+};
+
+export const caseFileTwo = {
   sight: "",
   hearing: "",
   movement: "",
@@ -55,40 +41,14 @@ export const caseFileInitialValues = {
   search: "",
 };
 
-export const caseFileObject = (caseFile) => {
+export const caseFileOneObject = (caseFile) => {
   return {
     ...caseFileInitialValues,
     ...caseFile,
-    ...caseFile?.clv,
-    ...caseFile?.complainant,
   };
 };
 
 export const caseFileSchema = Yup.object().shape({
-  search: Yup.string().required("Client Name is required"),
-  complainantName: Yup.string().required("Name is required"),
-  complainantSex: Yup.string().required("Sex is required"),
-  complainantAge: Yup.string().required("Age is required"),
-  complainantPhoneNumber: Yup.string().required("Phone Number is required"),
-  complainantCountry: Yup.string().required("Country is required"),
-  complainantNIN: Yup.string().required("NIN is required"),
-  complainantDistrict: Yup.string().required("District is required"),
-  complainantSubCounty: Yup.string().required("Sub County is required"),
-  complainantParish: Yup.string().required("Parish is required"),
-  complainantVillage: Yup.string().required("Village is required"),
-  complainantMaritalStatus: Yup.string().required("Marital Status is required"),
-  complainantAccompaniedBy: Yup.string().required("Accompanied by is required"),
-  complainantJob: Yup.string().required("Job is required"),
-  complainantPlaceOfWork: Yup.string().required("Place of work is required"),
-  complainantLevelOfEducation: Yup.string().required(
-    "Level of education is required"
-  ),
-  complainantPreferredLanguage: Yup.string().required(
-    "Preferred language is required"
-  ),
-  complainantRelationshipWithRespondent: Yup.string().required(
-    "Relationship is required"
-  ),
   respondentName: Yup.string().required("Name is required"),
   respondentSex: Yup.string().required("Sex is required"),
   respondentAge: Yup.string().required("Age is required"),
