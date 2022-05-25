d3a3b5c9f585b079637110e2a76212f705c1374c
nyabongoedgar <nyabongoedgar@gmail.com>
2022-05-25 12:07:29 +0300

    uncheck withform

--- src/hoc/withForm.jsx
+++ src/hoc/withForm.jsx
@@ -37,21 +37,20 @@ const withForm = (FormComponent) => {
         validationSchema={validationSchema}
         initialValues={initialValues}
         onSubmit={(values) => {
-          // if (isFormData) {
-          //   const formData = new FormData();
-          //   formData.append(fileName, file);
-          //   Object.keys(values).forEach((key) => {
-          //     formData.append(key, values[key]);
-          //   });
-          //   mutate(formData);
-          //   return;
-          // }
-          // if (isMutable) {
-          //   mutate(mutateData(values));
-          //   return;
-          // }
-          // mutate(values);
-          console.log(values)
+          if (isFormData) {
+            const formData = new FormData();
+            formData.append(fileName, file);
+            Object.keys(values).forEach((key) => {
+              formData.append(key, values[key]);
+            });
+            mutate(formData);
+            return;
+          }
+          if (isMutable) {
+            mutate(mutateData(values));
+            return;
+          }
+          mutate(values);
         }}
       >
         {({ values, setFieldValue, resetForm }) => (
