38ae3a766220395f0c6bcae85cbd011e7a2586e1
nyabongoedgar <nyabongoedgar@gmail.com>
2022-05-28 22:51:19 +0300

    update tracker table calculations

--- src/components/dashboard/TrackerTable/LeaveTrackerTable.js
+++ src/components/dashboard/TrackerTable/LeaveTrackerTable.js
@@ -34,8 +34,8 @@ const TrackerTable = ({ type, action, data, isLoading }) => {
                 return (
                   <Tr>
                     <Td className={classes.name_column}>{`${daysDifference(
-                      item.to,
-                      item.from
+                      item.from,
+                      item.to
                     )} days requested from ${getDay(item.to)} ${getMonthName(item.to)} to  ${getDay(item.from)} ${getMonthName(item.from)} `}</Td>
                     <Td className={classes.data__purpose_primary_text}>
                       {new Date(item.createdAt).toLocaleString()}
