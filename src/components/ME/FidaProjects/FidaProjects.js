3ac8623e0927274585f6925e36045077a942ffee
Moxhus <kimeramoses001@gmail.com>
2022-06-01 19:36:02 +0300

    Feat: Changed folders image, Ui fix on project files and loading state in tasks

--- src/components/ME/FidaProjects/FidaProjects.js
+++ src/components/ME/FidaProjects/FidaProjects.js
@@ -2,7 +2,6 @@ import { useDisclosure, useToast } from "@chakra-ui/react";
 import { useEffect } from "react";
 import Modal from "../../common/Modal";
 import SectionHeader from "../../common/SectionHeader";
-import TableSearch from "../../common/table/TableSearch";
 import FidaProjectTable from "./FidaProjectTable/FidaProjectTable";
 import NewFidaProjectForm from "./NewFidaProject/NewFidaProjectForm";
 import { useAddProject, useProjects } from "../../../hooks/useProjects";
@@ -30,8 +29,13 @@ const FidaProjects = () => {
   return (
     <>
       <SectionHeader title="Fida Projects" />
-      <TableSearch btnLabel="Add Project" btnClick={onOpen} />
-      {data?.projects && <FidaProjectTable data={data?.projects} />}
+      {data?.projects && (
+        <FidaProjectTable
+          data={data?.projects}
+          btnLabel="Add Project"
+          btnClick={onOpen}
+        />
+      )}
       <Modal isOpen={isOpen} onClose={onClose} title="Project Profiling Form">
         <NewFidaProjectForm
           isSubmitting={isSubmitting}
