d9acc8c67f23e14debf4b72087ccf4d2946794e3
conrad-evans <conradmugabe@gmail.com>
2022-06-07 22:05:00 +0300

    added report folder hooks

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -38,6 +38,7 @@ export const EMPLOYEES_KEY = "employees";
 export const ADVANCES_KEY = "advances";
 export const IT_PRODUCTS_KEY = "it_products";
 export const IT_SERVICES_KEY = "it_services";
+export const REPORT_FOLDERS = "report_folders";
 
 export const REQUISITIONS_STATS = "requisitions/stats";
 export const TRAVEL_ORDER_STATS = "travel_orders/stats";
