56d14f112410f6920f590fce9dd535d2ce1841e8
Moxhus <kimeramoses001@gmail.com>
2022-06-15 12:53:46 +0300

    update legal officer

--- src/components/CourtProcessServer/LegalOfficer/LegalOfficer.js
+++ src/components/CourtProcessServer/LegalOfficer/LegalOfficer.js
@@ -35,6 +35,17 @@ const data = [
     date: "15/APR/2022",
     clvExpiry: "15/APR/2020",
   },
+  {
+    name: "Namugambi Cynthia",
+    profession: "Dentist",
+    tel: "0759130054",
+    email: "cynthia@gmail.com",
+    address: "P.O Box 5569",
+    city: "Kampala",
+    active: true,
+    date: "15/APR/2022",
+    clvExpiry: "15/APR/2020",
+  },
 ];
 
 const LegalOfficer = () => {
