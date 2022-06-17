29b8e6cd045d4ee076cb8258e20e3f39f3a241c9
Muhumuza Fortune <fortunemuhumuza@gmail.com>
2022-06-17 05:10:00 +0300

    fix: add travel order form validation

--- src/components/forms/travelOrder/schemas/travelOrder.js
+++ src/components/forms/travelOrder/schemas/travelOrder.js
@@ -7,10 +7,12 @@ export const travelOrderSchema = yup.object().shape({
     .required(`Journey Start Time ${IS_REQUIRED}`),
   journey_end_time: yup.string().required(`Journey End Time ${IS_REQUIRED}`),
   purpose: yup.string().required(`Purpose ${IS_REQUIRED}`),
-  destination: yup.string().required(`Destination ${IS_REQUIRED}`),
+  // destination: yup.string().required(`Destination ${IS_REQUIRED}`),
   project: yup.string().required(`Project ${IS_REQUIRED}`),
   project_activity: yup.string().required(`Project Activity ${IS_REQUIRED}`),
-  pickup_location: yup.string().required(`Pickup Location ${IS_REQUIRED}`),
+  pickup_location: yup.string().required(`Pickup Location ` + IS_REQUIRED),
+  destination: yup.string().required("Destination is required"),
+  date_requested: yup.string().required("Date is required"),
 });
 
 export const travelOrderInitialValues = {
@@ -18,6 +20,7 @@ export const travelOrderInitialValues = {
   journey_end_time: "",
   purpose: "",
   destination: "",
+  date_requested: "",
   project: "",
   project_activity: "",
   pickup_location: "",
