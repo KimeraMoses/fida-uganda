ea4357405972eefefd2fa97b7a1db511a83c6600
Muhumuza Fortune <fortunemuhumuza@gmail.com>
2022-07-07 14:36:16 +0300

    fix: corrected typos of new member form

--- src/components/Membership/Members/NewMemberForm/schema.js
+++ src/components/Membership/Members/NewMemberForm/schema.js
@@ -4,22 +4,22 @@ import * as yup from "yup";
 export const initialValuesOneSchema = yup.object().shape({
   first_name: yup.string().required("First name is required"),
   last_name: yup.string().required("Last name is required"),
-  postal_address: yup.string().required("Postal address is required"),
-  phone_number: yup.string().required("Phone number is required"),
+  address: yup.string().required("Postal address is required"),
+  phoneNumber: yup.string().required("Phone number is required"),
   email: yup.string().required("Email is required"),
   employment_status: yup.string().required("Employment status is required"),
   year_of_undergraduate_completion: yup
     .string()
     .required("This field is required"),
-  professional_experience: yup.string().required("Professional experience is required"),
+  //professional_experience: yup.string().required("Professional experience is required"),
   membership_duration: yup.string().required("Membership duration is required"),
 });
 
 export const initialValuesOne = {
   first_name: "",
-  median_name: "",
+  maiden_name: "",
   last_name: "",
-  postal_address: "",
+  address: "",
   phoneNumber: "",
   email: "",
   employment_status: "",
