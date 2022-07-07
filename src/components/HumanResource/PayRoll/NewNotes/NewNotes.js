c25c44d10d550093736bff091acc0249cce03292
Muhumuza Fortune <fortunemuhumuza@gmail.com>
2022-07-07 14:39:32 +0300

    fix: add withForm to new notes form

--- src/components/HumanResource/PayRoll/NewNotes/NewNotes.js
+++ src/components/HumanResource/PayRoll/NewNotes/NewNotes.js
@@ -6,28 +6,14 @@ import TextField from "../../../common/TextField";
 import NumberField from "../../../common/NumberField";
 import SelectField from "../../../common/SelectField";
 import { monthsOptions } from "../../../../lib/options";
+import withForm from "../../../../hoc/withForm";
 
-const NewNotes = ({ onSubmit, isSubmitting }) => {
-  const [note, setNote] = React.useState("");
+const NewNotes = ({ onSubmit, isSubmitting, handleFileChange, file }) => {
 
-  const handleChange = (e) => {
-    const file = e.target.files[0];
-    setNote(file);
-  };
 
   return (
-    <Formik
-      initialValues={{ title: "", month: "", year: "", status: "unread" }}
-      onSubmit={(values) => {
-        const formData = new FormData();
-        formData.append("note", note);
-        Object.keys(values).forEach((key) => {
-          formData.append(key, values[key]);
-        });
-        onSubmit(formData);
-      }}
-    >
-      <SimpleGrid as={Form} p={5} gap={5}>
+ 
+      <SimpleGrid p={5} gap={5}>
         <TextField name="title" label="Title" placeholder="Title" />
         <SelectField
           name="month"
@@ -40,14 +26,13 @@ const NewNotes = ({ onSubmit, isSubmitting }) => {
           <Heading fontSize="md" fontWeight="medium">
             Select File:
           </Heading>
-          <input type="file" onChange={handleChange} />
+          <input type="file" onChange={handleFileChange} />
         </Flex>
         <FormButton isSubmitting={isSubmitting} type="submit">
           Upload Note
         </FormButton>
       </SimpleGrid>
-    </Formik>
   );
 };
 
-export default NewNotes;
+export default withForm(NewNotes);
