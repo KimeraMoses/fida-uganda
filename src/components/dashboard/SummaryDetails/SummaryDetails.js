1d38e1218144f62450cab9164d63aeadd2bda60c
KimeraMoses <kimeramoses001@gmail.com>
2022-07-17 01:06:51 +0300

    made changes

--- src/components/dashboard/SummaryDetails/SummaryDetails.js
+++ src/components/dashboard/SummaryDetails/SummaryDetails.js
@@ -10,8 +10,8 @@ import {
   useLeaveRequest,
   // useApproveLeaveRequest,
 } from "../../../hooks/useLeaveRequest";
-import {useTravelOrder} from "../../../hooks/useTravelOrders";
-import {useRequisition} from "../../../hooks/useRequisitions";
+import { useTravelOrder } from "../../../hooks/useTravelOrders";
+import { useRequisition } from "../../../hooks/useRequisitions";
 import Loader from "../../common/UI/Loader/Loader";
 
 // export const TravelData = [
@@ -165,25 +165,27 @@ const SummaryDetails = (props) => {
   const selectedType = query.get("application-type");
   const id = query.get("id");
 
-    const {travelName} = useParams();
+  const { travelName } = useParams();
   // console.log(travelName)
 
-  const {reqName}= useParams();
+  const { reqName } = useParams();
   // console.log(reqName)
 
-   const { data, isLoading } = useAdvance(id);
-   // console.log(data);
+  console.log("Summary Details Reloaded");
+
+  const { data, isLoading } = useAdvance(id);
+  console.log(data);
 
   const { data: leaveData, isLoading: loadingLeaveRequests } =
     useLeaveRequest(id);
   // console.log(leaveData)
 
-  const {data: travelData, isLoading: loadingTravelOrders} =  useTravelOrder(travelName);
-   console.log(travelData)
+  const { data: travelData, isLoading: loadingTravelOrders } =
+    useTravelOrder(travelName);
+  // console.log(travelData);
 
-  const {data:reqData, isLoading: loadingReqData} = useRequisition(reqName);
-   // console.log(reqData)
-
+  const { data: reqData, isLoading: loadingReqData } = useRequisition(reqName);
+  // console.log(reqData)
 
   return (
     <div className={classes.summary_wrapper}>
@@ -206,9 +208,7 @@ const SummaryDetails = (props) => {
             ? "Requisition Summary"
             : type === "travel"
             ? "Travel Order Summary"
-            : ` ${
-                selectedType === "leave" ? "Leave" : "Advance"
-              } Application`}
+            : ` ${selectedType === "leave" ? "Leave" : "Advance"} Application`}
         </h1>
       </div>
       <div className={classes.summary_details_wrapper}>
@@ -224,93 +224,127 @@ const SummaryDetails = (props) => {
           </h2>
 
           <div className={classes.user_details}>
-            {(isLoading && selectedType==="advance") ?  <Loader/>: !isLoading && data && selectedType !== "leave" && (
-              <SimpleGrid columns={2} spacing={1}>
-                <h6>Date of Application:</h6>
-                <h6>{new Date(data?.advance?.createdAt).toLocaleString()}</h6>
-                <h6>Name:</h6>
-                <h6>{data?.advance?.user?.full_name}</h6>
-                <h6>Designation:</h6>
-                <h6>{data?.advance?.user?.designation}</h6>
-                <h6>Budget Year:</h6>
-                <h6>{data?.advance?.budget_year}</h6>
-                <h6>Month for Application:</h6>
-                <h6>{data?.advance?.month}</h6>
-                <h6>Net Salary:</h6>
-                <h6>{data?.advance?.net_pay}</h6>
-                <h6>Advance:</h6>
-                <h6>{data?.advance?.amount}</h6>
-                <h6>Reason:</h6>
-                <h6>{data?.advance?.reason}</h6>
-              </SimpleGrid>
+            {isLoading && selectedType === "advance" ? (
+              <Loader />
+            ) : (
+              !isLoading &&
+              data &&
+              selectedType !== "leave" && (
+                <SimpleGrid columns={2} spacing={1}>
+                  <h6>Date of Application:</h6>
+                  <h6>{new Date(data?.advance?.createdAt).toLocaleString()}</h6>
+                  <h6>Name:</h6>
+                  <h6>{data?.advance?.user?.full_name}</h6>
+                  <h6>Designation:</h6>
+                  <h6>{data?.advance?.user?.designation}</h6>
+                  <h6>Budget Year:</h6>
+                  <h6>{data?.advance?.budget_year}</h6>
+                  <h6>Month for Application:</h6>
+                  <h6>{data?.advance?.month}</h6>
+                  <h6>Net Salary:</h6>
+                  <h6>{data?.advance?.net_pay}</h6>
+                  <h6>Advance:</h6>
+                  <h6>{data?.advance?.amount}</h6>
+                  <h6>Reason:</h6>
+                  <h6>{data?.advance?.reason}</h6>
+                </SimpleGrid>
+              )
             )}
-            {(loadingReqData && type === "requisition")? <Loader/>: !loadingReqData && reqName && type === "requisition" && (
-              <SimpleGrid columns={2} spacing={1}>
-                <h6>Project Name:</h6>
-                <h6>{reqData?.requisition?.project_name}</h6>
-                <h6>Budget year </h6>
-                <h6>{reqData?.requisition?.budget_year}</h6>
-                <h6>Type: </h6>
-                <h6>{reqData?.requisition?.type}</h6>
-                <h6>Unit Price:</h6>
-                <h6>{reqData?.requisition?.unit_price}</h6>
-                <h6>Number of Units:</h6>
-                <h6>{reqData?.requisition?.num_units}</h6>
-                <h6>Total Amount:</h6>
-                {/*total amount not included*/}
-                <h6>45</h6>
-                <h6>Subject of Procurement:</h6>
-                <h6>{reqData?.requisition?.subject_of_procurement}</h6>
-                <h6>Date required:</h6>
-                <h6>{new Date(reqData?.requisition?.date_required).toLocaleString()}</h6>
-                <h6>Delivery Location:</h6>
-                <h6>{reqData?.requisition?.delivery_location}</h6>
-              </SimpleGrid>
-            )}
-
-            {(loadingTravelOrders && type === "travel")? <Loader/>: !loadingTravelOrders && travelData && type === "travel" && (
-              <SimpleGrid columns={2} spacing={1}>
-                <h6>Date requested:</h6>
-                <h6>{new Date(travelData?.travelOrder?.createdAt).toLocaleString()}</h6>
-                <h6>Journey start time:</h6>
-                <h6>{travelData?.travelOrder?.journey_start_time}</h6>
-                <h6>Journey end time: </h6>
-                <h6>{travelData?.travelOrder?.journey_end_time}</h6>
-                <h6>Pick up location:</h6>
-                <h6>{travelData?.travelOrder?.pickup_location}</h6>
-                <h6>Destination:</h6>
-                <h6>{travelData?.travelOrder?.destination}</h6>
-                <h6>Project:</h6>
-                <h6>{travelData?.travelOrder?.project}</h6>
-                <h6>Project Activity:</h6>
-                <h6>{travelData?.travelOrder?.project_activity}</h6>
-                <h6>Purpose of Activity:</h6>
-                <h6>{travelData?.travelOrder?.purpose}</h6>
-                <h6>Date required:</h6>
-                {/*Not sure of the date required field name*/}
-                <h6>{new Date(travelData?.travelOrder?.updateAt).toLocaleString()}</h6>
-              </SimpleGrid>
+            {loadingReqData && type === "requisition" ? (
+              <Loader />
+            ) : (
+              !loadingReqData &&
+              reqName &&
+              type === "requisition" && (
+                <SimpleGrid columns={2} spacing={1}>
+                  <h6>Project Name:</h6>
+                  <h6>{reqData?.requisition?.project_name}</h6>
+                  <h6>Budget year </h6>
+                  <h6>{reqData?.requisition?.budget_year}</h6>
+                  <h6>Type: </h6>
+                  <h6>{reqData?.requisition?.type}</h6>
+                  <h6>Unit Price:</h6>
+                  <h6>{reqData?.requisition?.unit_price}</h6>
+                  <h6>Number of Units:</h6>
+                  <h6>{reqData?.requisition?.num_units}</h6>
+                  <h6>Total Amount:</h6>
+                  <h6>45</h6>
+                  <h6>Subject of Procurement:</h6>
+                  <h6>{reqData?.requisition?.subject_of_procurement}</h6>
+                  <h6>Date required:</h6>
+                  <h6>
+                    {new Date(
+                      reqData?.requisition?.date_required
+                    ).toLocaleString()}
+                  </h6>
+                  <h6>Delivery Location:</h6>
+                  <h6>{reqData?.requisition?.delivery_location}</h6>
+                </SimpleGrid>
+              )
             )}
 
-            {(loadingLeaveRequests && selectedType === "leave")? <Loader/>: !loadingLeaveRequests && leaveData && selectedType === "leave" && (
-              <SimpleGrid columns={2} spacing={1}>
-                <h6>Date of Application:</h6>
-                <h6>
-                  {new Date(leaveData?.leave?.createdAt).toLocaleString()}
-                </h6>
-                <h6>Name:</h6>
-                <h6>{leaveData?.leave?.user?.full_name}</h6>
-                <h6>Designation:</h6>
-                <h6>{leaveData?.leave?.user?.designation}</h6>
-                <h6>I wish to apply for:</h6>
-                <h6>{selectedType}</h6>
-                <h6>Reason:</h6>
-                <h6>{leaveData?.leave?.reason}</h6>
-                <h6>While on leave my physical contact will be:</h6>
-                <h6>{leaveData?.leave?.address_on_leave}</h6>
-                <h6>Tel:</h6>
-                <h6>{leaveData?.leave?.tel_on_leave}</h6>
-              </SimpleGrid>
+            {loadingTravelOrders && type === "travel" ? (
+              <Loader />
+            ) : (
+              !loadingTravelOrders &&
+              travelData &&
+              type === "travel" && (
+                <SimpleGrid columns={2} spacing={1}>
+                  <h6>Date requested:</h6>
+                  <h6>
+                    {new Date(
+                      travelData?.travelOrder?.createdAt
+                    ).toLocaleString()}
+                  </h6>
+                  <h6>Journey start time:</h6>
+                  <h6>{travelData?.travelOrder?.journey_start_time}</h6>
+                  <h6>Journey end time: </h6>
+                  <h6>{travelData?.travelOrder?.journey_end_time}</h6>
+                  <h6>Pick up location:</h6>
+                  <h6>{travelData?.travelOrder?.pickup_location}</h6>
+                  <h6>Destination:</h6>
+                  <h6>{travelData?.travelOrder?.destination}</h6>
+                  <h6>Project:</h6>
+                  <h6>{travelData?.travelOrder?.project}</h6>
+                  <h6>Project Activity:</h6>
+                  <h6>{travelData?.travelOrder?.project_activity}</h6>
+                  <h6>Purpose of Activity:</h6>
+                  <h6>{travelData?.travelOrder?.purpose}</h6>
+                  <h6>Date required:</h6>
+                  <h6>
+                    {new Date(
+                      travelData?.travelOrder?.updateAt
+                    ).toLocaleString()}
+                  </h6>
+                </SimpleGrid>
+              )
+            )}
+
+            {loadingLeaveRequests && selectedType === "leave" ? (
+              <Loader />
+            ) : (
+              !loadingLeaveRequests &&
+              leaveData &&
+              selectedType === "leave" && (
+                <SimpleGrid columns={2} spacing={1}>
+                  <h6>Date of Application:</h6>
+                  <h6>
+                    {new Date(leaveData?.leave?.createdAt).toLocaleString()}
+                  </h6>
+                  <h6>Name:</h6>
+                  <h6>{leaveData?.leave?.user?.full_name}</h6>
+                  <h6>Designation:</h6>
+                  <h6>{leaveData?.leave?.user?.designation}</h6>
+                  <h6>I wish to apply for:</h6>
+                  <h6>{selectedType}</h6>
+                  <h6>Reason:</h6>
+                  <h6>{leaveData?.leave?.reason}</h6>
+                  <h6>While on leave my physical contact will be:</h6>
+                  <h6>{leaveData?.leave?.address_on_leave}</h6>
+                  <h6>Tel:</h6>
+                  <h6>{leaveData?.leave?.tel_on_leave}</h6>
+                </SimpleGrid>
+              )
             )}
           </div>
         </div>
@@ -331,13 +365,13 @@ const SummaryDetails = (props) => {
           />
         </div>
         <form>
-        <div className={classes.remarks_wrapper}>
-          <h6>
-            <strong>Remarks</strong>
-          </h6>
-          <Textarea placeholder="Leave a remark here" />
-        </div>
-        <hr/>
+          <div className={classes.remarks_wrapper}>
+            <h6>
+              <strong>Remarks</strong>
+            </h6>
+            <Textarea placeholder="Leave a remark here" />
+          </div>
+          <hr />
           <div className={classes.form_action_wrapper}>
             <FormButton variant="cancel" type="button">
               Reject
