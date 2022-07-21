24b1497c88d75acc16b64a8885669ba6249a0f0f
conrad-evans <conradmugabe@gmail.com>
2022-07-21 15:37:49 +0300

    fixed error display issue

--- src/lib/toastDetails.js
+++ src/lib/toastDetails.js
@@ -1,22 +1,22 @@
 export const toastError = (error) => {
-  const GENERIC_ERROR = "An Error Occurred";
+  const GENERIC_ERROR = 'An Error Occurred';
 
   return {
-    title: "Error",
-    description: typeof error === "string" ? error : GENERIC_ERROR,
-    status: "error",
+    title: 'Error',
+    description: typeof error === 'string' ? error : GENERIC_ERROR,
+    status: 'error',
     duration: 9000,
     isClosable: true,
   };
 };
 
 export const toastSuccess = (message) => {
-  const GENERIC_MESSAGE = "Action successful";
+  const GENERIC_MESSAGE = 'Action successful';
 
   return {
-    title: "Success",
-    description: typeof message === "string" ? message : GENERIC_MESSAGE,
-    status: "success",
+    title: 'Success',
+    description: typeof message === 'string' ? message : GENERIC_MESSAGE,
+    status: 'success',
     duration: 4000,
     isClosable: true,
   };
