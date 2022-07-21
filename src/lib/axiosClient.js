dd258b3faf3e4062b46d81601595e658d99999a2
conrad-evans <conradmugabe@gmail.com>
2022-07-21 15:37:49 +0300

    fixed error display issue

--- src/lib/axiosClient.js
+++ src/lib/axiosClient.js
@@ -1,8 +1,9 @@
-import axios from "axios";
-import { toast } from "react-toastify";
+import axios from 'axios';
+import { queryCache } from 'react-query';
+import { toast } from 'react-toastify';
 
 const onRequest = (config) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
   if (config.headers) {
     config.headers.Authorization = `Bearer ${token}`;
   } else {
@@ -22,35 +23,42 @@ const onResponse = (response) => {
 };
 
 const onResponseError = (error) => {
-  console.log(error);
-  if (error.message === "Network Error") {
-    toast.error("Network Error - Please check your connection");
+  if (error.message === 'Network Error') {
+    toast.error('Network Error - Please check your connection');
   }
   const { status, config } = error.response;
   if (status === 401) {
     // redirect the user to the sign in page
     // remember to add the redirect into the history array of the react-router-dom
-    // console.log("401");
+    queryCache?.clear();
+    window.localStorage.removeItem('token');
+    window.location.assign('/');
+    toast.error('Please login again');
+    return Promise.reject(error.response?.data.message);
   }
-  if (status === 400 || config.method === "get") {
+  if (status === 400 || config.method === 'get') {
     // console.log("400 or get");
+    return Promise.reject(error.response?.data.message);
   }
   if (status === 404) {
     // redirect the user to the 404 page (Not Found)
     // console.log("404");
+    // redirect user to route not found
+    return Promise.reject(error.response?.data.message);
   }
 
   if (status === 500) {
     // display toast to let user know something went wrong
     // ask them to try again.
-    // toast.error("Sorry, something went wrong on your end. - Please try again!");
+    toast.error('Sorry, something went wrong on your end. - Please try again!');
+    return Promise.reject(error.response?.data.message);
   }
   // else {
   // this is for all other status codes that failed
   //   toast.error("Something went wrong - Please try again!");
   // }
 
-  // return Promise.reject(error.response?.data.message);
+  return Promise.reject(error.response?.data.message);
 };
 
 const setupInterceptorsTo = (axiosInstance) => {
