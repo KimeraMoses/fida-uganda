c4f9ab2a47b9cda13c9d7212481c1551225333e8
conrad-evans <conradmugabe@gmail.com>
2022-07-25 09:46:12 +0300

    changed the sign in flow logic to fix the re-rendering of the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,22 @@
-import { useEffect, useMemo } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useGetMe } from "./hooks/useUser";
-import Auth from "./components/compound/Auth";
-import LoadingPage from "./components/common/LoadingPage";
-import View from "./View";
-import { loginUser } from "./store/authReducer";
-import useOnlineStatus from "./hooks/useOnlineStatus";
-import { toast } from "react-toastify";
+import { useDispatch } from 'react-redux';
+import { useGetMe } from './hooks/useUser';
+import Auth from './components/compound/Auth';
+import LoadingPage from './components/common/LoadingPage';
+import View from './View';
+import { loginUser } from './store/authReducer';
 
 function App() {
-  const { refetch, data, isLoading } = useGetMe();
-  const { user } = useSelector((state) => state.auth);
+  const { status, user: userData } = useGetMe();
   const dispatch = useDispatch();
-  const userData = useMemo(() => data, [data]);
-  const isOnline = useOnlineStatus();
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      refetch();
-    }
-    if (userData) {
-      dispatch(loginUser(userData));
-    }
-    if (!isOnline) {
-      // raise a toast to tell the user that they're offline
-      toast.warn("You're offline. Check your internet connection");
-    }
-  }, [refetch, dispatch, userData, isOnline]);
-
-  if (!user) {
-    if (isLoading) {
+  if (!userData) {
+    if (status === 'loading' || status === 'idle') {
       return <LoadingPage />;
     }
     return <Auth />;
   }
 
+  dispatch(loginUser(userData));
   return <View />;
 }
 
