f33ed909eddfb57524f9c1b597ca6d28b4bcc3c5
KimeraMoses <kimeramoses001@gmail.com>
2022-08-03 09:53:43 +0300

    Fix: fixed travel order

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://206.189.134.224/api/v1";
+export const BASE_URL = "https://fida-ims-staging-v2.herokuapp.com/api/v1";
 export const TEN_MBS_IN_BYTES = 10485760;
 
 export const USERS_KEY = "users";
