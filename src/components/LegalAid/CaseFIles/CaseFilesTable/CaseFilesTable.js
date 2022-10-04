63399e2c38614d5a1f8ead6d056d511d812e5868
KimeraMoses <kimeramoses001@gmail.com>
2022-10-04 09:19:41 +0300

    fix: case files crashing

--- src/components/LegalAid/CaseFIles/CaseFilesTable/CaseFilesTable.js
+++ src/components/LegalAid/CaseFIles/CaseFilesTable/CaseFilesTable.js
@@ -22,7 +22,6 @@ import { MdOutlineRemoveRedEye } from "react-icons/md";
 const CaseFilesTable = ({ data }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const dispatch = useDispatch();
-
   const onHandleClick = (caseFile) => {
     dispatch(selectCaseFile(caseFile));
     onOpen();
@@ -57,31 +56,76 @@ const CaseFilesTable = ({ data }) => {
               return (
                 <Tr key={item.id} onClick={() => onHandleClick(item)}>
                   <Td>{item.case_id}</Td>
-                  <Td>{item.complainant?.name}</Td>
-                  <Td>{item.complainant?.village}</Td>
+                  <Td>
+                    {item.complainant?.name ? item.complainant?.name : "N/A"}
+                  </Td>
+                  <Td>
+                    {item.complainant?.village
+                      ? item.complainant?.village
+                      : "N/A"}
+                  </Td>
                   {/* <Td>{item.district}</Td> */}
-                  <Td>{item.complainant?.country}</Td>
-                  <Td>{item.complainant?.nin}</Td>
-                  <Td>{item.file_opened}</Td>
+
+                  <Td>
+                    {item.complainant?.country
+                      ? item.complainant?.country
+                      : "N/A"}
+                  </Td>
+                  <Td>
+                    {item.complainant?.NIN ? item.complainant?.NIN : "N/A"}
+                  </Td>
+
+                  <Td>{item?.file_opened ? item?.file_opened : "N/A"}</Td>
+
                   <Td>{formatDate(item.createdAt)}</Td>
                   <Td>{item.case_id}</Td>
-                  <Td>{item.complainant?.sex}</Td>
-                  <Td>{item.complainant?.age}</Td>
-                  <Td>{item.complainant?.num_beneficiaries}</Td>
-                  <Td>{item.complainant?.occupation}</Td>
-                  <Td>{item.nature}</Td>
-                  <Td>{item.action_taken}</Td>
-                  <Td>{item.next_visit}</Td>
-                  <Td>{item.legal_officer}</Td>
-                  <Td>{item.referred_to}</Td>
-                  <Td>{item.reason_for_referral}</Td>
-                  <Td>{item.follow_up_feedback}</Td>
-                  <Td>{formatDate(item.createdAt)}</Td>
-                  <Td>{item.respondent_contact}</Td>
-                  <Td>{item.complainant?.disability}</Td>
-                  <Td>{item.respondentName}</Td>
-                  <Td>{item.contact}</Td>
-                  <Td>{item.fida}</Td>
+                  <Td>
+                    {item.complainant?.sex ? item.complainant?.sex : "N/A"}
+                  </Td>
+                  <Td>
+                    {item.complainant?.age ? item.complainant?.age : "N/A"}
+                  </Td>
+                  <Td>{item.complainant?.beneficiaries?.length}</Td>
+                  <Td>
+                    {item.complainant?.occupation
+                      ? item.complainant?.occupation
+                      : "N/A"}
+                  </Td>
+                  <Td>{item?.nature ? item?.nature : "N/A"}</Td>
+                  <Td>{item?.actionsTaken?.length}</Td>
+                  <Td>{item?.next_visit ? item?.next_visit : "N/A"}</Td>
+                  <Td>{item?.legal_officer ? item?.legal_officer : "N/A"}</Td>
+                  <Td>
+                    {item?.referred_to?.full_name
+                      ? item?.referred_to?.full_name
+                      : "N/A"}
+                  </Td>
+                  <Td>
+                    {item?.reason_for_referral
+                      ? item?.reason_for_referral
+                      : "N/A"}
+                  </Td>
+                  <Td>
+                    {item?.follow_up_feedback
+                      ? item?.follow_up_feedback
+                      : "N/A"}
+                  </Td>
+                  <Td>{formatDate(item.updateAt)}</Td>
+                  <Td>
+                    {item?.respondent_contact
+                      ? item?.respondent_contact
+                      : "N/A"}
+                  </Td>
+                  <Td>
+                    {item?.complainant?.disability
+                      ? item?.complainant?.disability
+                      : "N/A"}
+                  </Td>
+                  <Td>{item?.respondentName}</Td>
+                  <Td>
+                    {item?.respondentPhone ? item?.respondentPhone : "N/A"}
+                  </Td>
+                  <Td>{item?.fida ? item?.fida : "N/A"}</Td>
                   <Td>{formatDate(item.createdAt)}</Td>
                   <Td style={{ textAlign: "center" }}>
                     <div className={classes.table_actions_icon_wrapper}>
