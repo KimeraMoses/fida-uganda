58b4e8f3023c3b2c729207f3fe9312f3bef3bcde
KimeraMoses <kimeramoses001@gmail.com>
2022-10-11 10:47:53 +0300

    new:table refactored to make it reusable

--- src/components/HumanResource/FidaApprovals/FidaApproved.js
+++ src/components/HumanResource/FidaApprovals/FidaApproved.js
@@ -1,37 +1,44 @@
 import React, { useEffect, useState } from "react";
-import { useActivatedUsers } from "../../../hooks/useUser";
+import {
+  useActivatedUsers,
+  useRequestPasswordLink,
+} from "../../../hooks/useUser";
 import SubHeading from "./../../Tasks/SubHeading/SubHeading";
 import Loader from "./../../common/UI/Loader/Loader";
 import { formatDate } from "../../../lib/data";
 import Table from "../../common/TableComponent/Table";
+import Modal from "../../common/Modal";
+import EmployeeCard from "./NewEmployeeForm/EmployeeCard";
+import { useDisclosure } from "@chakra-ui/react";
+
 // import CustomTable from ""
-const columns = [
+export const ApprovalColumns = [
   {
     Header: "Name",
-    accessor: "name",
+    accessor: "full_name",
   },
   {
     Header: "Email",
     accessor: "email",
   },
   {
-    Header: "Role",
-    accessor: "role",
-  },
-  {
     Header: "Designation",
     accessor: "designation",
   },
   {
-    Header: "Project of Attachement",
+    Header: "Sign Up Date",
     accessor: "createdAt",
+    Cell: ({ cell: { value } }) => formatDate(value),
   },
 ];
 
 const FidaApproved = () => {
   const { data: userData, isLoading } = useActivatedUsers();
-  // Setting data properly
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isSuccess, isError, error } = useRequestPasswordLink();
+
   const [data, setData] = useState([]);
+  const [user, setUser] = useState({});
   useEffect(() => {
     setData([]);
     if (userData?.users?.length) {
@@ -39,13 +46,24 @@ const FidaApproved = () => {
         return {
           ...b,
           key: b?.id,
-          name: b?.first_name ? `${b?.first_name} ${b?.last_name}` : "N/A",
-          createdAt: formatDate(b?.createdAt),
+          full_name: b?.full_name ? b.full_name : "N/A",
         };
       });
       setData(dataToSet);
     }
   }, [userData]);
+
+  useEffect(() => {
+    if (isSuccess) {
+      // toast(toastSuccess("User activated successfully"));
+      onClose();
+    }
+  }, [isSuccess, onClose]);
+
+  const onEditHandler = (user) => {
+    setUser(user);
+    onOpen();
+  };
 
   return (
     <>
@@ -54,9 +72,26 @@ const FidaApproved = () => {
       ) : (
         <>
           <SubHeading title="Approved Users" />
-          <Table data={data} columns={columns} />
+          <Table
+            data={data}
+            columns={ApprovalColumns}
+            showBtn={false}
+            loading={isLoading}
+            showActions={true}
+            onEditHandler={onEditHandler}
+          />
         </>
       )}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <EmployeeCard
+          // isSubmitting={isLoading}
+          onClose={onClose}
+          user={user}
+          // onSubmit={mutate}
+          isError={isError}
+          error={error}
+        />
+      </Modal>
     </>
   );
 };
--- src/components/LegalAid/CaseFIles/CaseFiles.js
+++ src/components/LegalAid/CaseFIles/CaseFiles.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import SectionHeader from '../../common/SectionHeader';
-import { useDisclosure } from '@chakra-ui/react';
-import CaseFilesTable from './CaseFilesTable/CaseFilesTable';
-import Modal from '../../common/Modal';
-import NewCaseFile from './NewCaseFile/NewCaseFile';
-import { useCaseFiles } from '../../../hooks/useCaseFiles';
-import { useDispatch } from 'react-redux';
-import { resetCaseFile } from '../../../store/caseFileReducer';
-import Loader from './../../common/UI/Loader/Loader';
+import React, { useState } from "react";
+import SectionHeader from "../../common/SectionHeader";
+import { useDisclosure } from "@chakra-ui/react";
+import Modal from "../../common/Modal";
+import NewCaseFile from "./NewCaseFile/NewCaseFile";
+import { useCaseFiles } from "../../../hooks/useCaseFiles";
+import { useDispatch } from "react-redux";
+import { resetCaseFile, selectCaseFile } from "../../../store/caseFileReducer";
+import Loader from "./../../common/UI/Loader/Loader";
+import Table from "../../common/TableComponent/Table";
+import { formatDate } from "../../../lib/data";
+
+export const caseColumns = [
+  { Header: "File No.", accessor: "case_id" },
+  { Header: "Name", accessor: "complainant.name" },
+  { Header: "SubCounty", accessor: "complainant.village" },
+  // { Header: "District", accessor: "districtOfOrigin" },
+  { Header: "Nationality", accessor: "complainant.country" },
+  { Header: "NIN", accessor: "complainant.NIN" },
+  // { Header: "File type", accessor: "fileType" },
+  {
+    Header: "Date of opening",
+    accessor: "createdAt",
+    Cell: ({ cell: { value } }) => formatDate(value),
+  },
+  // { Header: "Case Number", accessor: "caseNumber" },
+  { Header: "Gender", accessor: "complainant.sex" },
+  { Header: "Age", accessor: "complainant.age" },
+  {
+    Header: "No. of beneficiaries",
+    accessor: "complainant.beneficiaries",
+    Cell: ({ cell: { value } }) => value?.length,
+  },
+  { Header: "Occupation", accessor: "complainant.occupation" },
+
+  { Header: "Nature of problem", accessor: "nature" },
+  {
+    Header: "Action taken",
+    accessor: "actionsTaken",
+    Cell: ({ cell: { value } }) => value?.length,
+  },
+  { Header: "Next visit", accessor: "next_visit" },
+  { Header: "Legal Officer", accessor: "legal_officer" },
+  { Header: "Referred by", accessor: "referred_to.full_name" },
+  { Header: "Reason for referral", accessor: "reason_for_referral" },
+  { Header: "Follow up feedback", accessor: "follow_up_feedback" },
+  {
+    Header: "File closing date",
+    accessor: "updateAt",
+    Cell: ({ cell: { value } }) => formatDate(value),
+  },
+  { Header: "Phone Number", accessor: "respondent_contact" },
+  { Header: "Disability", accessor: "complainant.disability" },
+  { Header: "Respondent Name", accessor: "respondentName" },
+  { Header: "Respondent Phone Number", accessor: "respondentPhone" },
+  { Header: "How did you find fida", accessor: "fida" },
+  // {
+  //   Header: "Date Uploaded",
+  //   accessor: "dateUploaded",
+  //   Cell: ({ cell: { value } }) => formatDate(value),
+  // },
+];
 
 const CaseFiles = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { data, isLoading } = useCaseFiles();
+  const [isEdit, setIsEdit] = useState(false);
   const dispatch = useDispatch();
 
   const onCloseModal = () => {
@@ -22,7 +75,16 @@ const CaseFiles = () => {
   const onOpenModal = () => {
     onOpen();
     dispatch(resetCaseFile());
+    setIsEdit(false);
   };
+
+  const onHandleClick = (caseFile) => {
+    setIsEdit(true);
+    dispatch(selectCaseFile(caseFile));
+    onOpen();
+  };
+
+  console.log("case file", data);
   return (
     <>
       <SectionHeader title="Case Files" />
@@ -30,11 +92,14 @@ const CaseFiles = () => {
         <Loader />
       ) : (
         data?.cases && (
-          <CaseFilesTable
-            data={data ? data.cases : null}
-            btnLabel="New Case File"
+          <Table
+            data={data.cases}
+            columns={caseColumns}
             btnClick={onOpenModal}
+            btnLabel="New Case File"
             tableName="Case Files"
+            loading={isLoading}
+            onEditHandler={onHandleClick}
           />
         )
       )}
@@ -47,7 +112,7 @@ const CaseFiles = () => {
         <NewCaseFile
           isClvCaseFile={false}
           onClose={onCloseModal}
-          isNewCaseFile={true}
+          isNewCaseFile={isEdit ? false : true}
         />
       </Modal>
     </>
