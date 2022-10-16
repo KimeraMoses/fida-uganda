1a408300b93760e1a8f6f69509c3cc8cb14ac120
Muhumuza Fortune <fortunemuhumuza@gmail.com>
2022-10-16 22:21:47 +0300

    fix: add pagination to it department tables

--- src/components/itDepartment/Products/ITProducts.js
+++ src/components/itDepartment/Products/ITProducts.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDisclosure } from "@chakra-ui/react";
 import SectionHeader from "../../common/SectionHeader";
 import ITProductForm from "../../forms/it/AddITProductForm";
@@ -10,10 +10,42 @@ import Modal from "../../common/Modal";
 import ITProductsTable from "./ITProductsTable";
 import { useAddItProduct, useItProducts } from "../../../hooks/useItProduct";
 import Loader from "../../common/UI/Loader/Loader";
+import Table from "../../common/TableComponent/Table";
+import { itProductsColumns } from "../../../lib/tableColumns";
 
 const ITProducts = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const { data, isLoading } = useItProducts();
+  const { data: itProductsData, isLoading } = useItProducts();
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    setData([]);
+    if (itProductsData?.ITProducts?.length) {
+      const dataToSet = itProductsData?.ITProducts?.map((b) => {
+        return {
+          ...b,
+          name: {
+            name: b?.brand,
+            location: b?.delivery_location,
+          },
+          category: {
+            category: b?.category ? b?.category : "N/A",
+            class: b?.class,
+          },
+          amount: {
+            amount: b?.amount,
+            currency: b?.currency
+          },
+            payment_status:{
+              payment_status:b?.payment_status ? b?.payment_status : 'N/A',
+              purchase_date: b?.purchase_date ? b?.purchase_date : 'N/A'
+            }
+        };
+      });
+      console.log('it data', dataToSet)
+      setData(dataToSet);
+    }
+  }, [itProductsData]);
 
   return (
     <>
@@ -21,29 +53,37 @@ const ITProducts = () => {
       {isLoading ? (
         <Loader />
       ) : (
-        <ITProductsTable
-          data={data?.ITProducts}
-          isProducts={true}
-          isLoading={isLoading}
-          btnLabel="Add Product"
-          btnClick={onOpen}
-          keysToFilterOut={[
-            "project_name",
-            "brand",
-            "id",
-            "createdAt",
-            "ITProduct_roles",
-            "updateAt",
-            "date_required",
-            "budget_year",
-            "name",
-            "currency",
-            "class",
-            "status",
-            "registeredBy",
-          ]}
-          tableName="IT Products"
-        />
+        <Table
+        isLoading={isLoading}
+        data={data ? data : null}
+        btnLabel="Add Product"
+        tableName="IT Products"
+        columns={itProductsColumns}
+      />
+        
+        // <ITProductsTable
+        //   data={data?.ITProducts}
+        //   isProducts={true}
+        //   isLoading={isLoading}
+        //   btnLabel="Add Product"
+        //   btnClick={onOpen}
+        //   keysToFilterOut={[
+        //     "project_name",
+        //     "brand",
+        //     "id",
+        //     "createdAt",
+        //     "ITProduct_roles",
+        //     "updateAt",
+        //     "date_required",
+        //     "budget_year",
+        //     "name",
+        //     "currency",
+        //     "class",
+        //     "status",
+        //     "registeredBy",
+        //   ]}
+        //   tableName="IT Products"
+        // />
       )}
       <Modal
         isOpen={isOpen}
