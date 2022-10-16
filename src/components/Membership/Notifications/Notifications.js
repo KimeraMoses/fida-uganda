d0f653e6d49341b9d361bbb0304e958a810c3251
Muhumuza Fortune <fortunemuhumuza@gmail.com>
2022-10-16 22:19:24 +0300

    refactor: refactor notifications table

--- src/components/Membership/Notifications/Notifications.js
+++ src/components/Membership/Notifications/Notifications.js
@@ -2,16 +2,17 @@ import { useDisclosure } from "@chakra-ui/react";
 import React from "react";
 import SectionHeader from "../../common/SectionHeader";
 import Form from "./NotificationForm";
-import NotificationsTable from "./NotificationsTable/NotificationTable";
 import { useMembers } from "../../../hooks/useMember";
 import {
   useAddNotification,
   useNotifications,
 } from "../../../hooks/useNotification";
 import { notificationSchema } from "./schema";
+import { notificationsTableColumns } from "../../../lib/tableColumns";
+import Table from "../../common/TableComponent/Table";
 
 const Notifications = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen,  onClose } = useDisclosure();
   const { data: membersData } = useMembers();
   const { data, isLoading } = useNotifications();
 
@@ -24,13 +25,19 @@ const Notifications = () => {
     <div>
       <SectionHeader title="Notifications" />
       {/* <TableSearch btnLabel="Compose" btnClick={onOpen} /> */}
-      <NotificationsTable
+      <Table
+            data={data ? data.Notifications : null}
+            columns={notificationsTableColumns}
+        btnLabel="Compose"
+            loading={isLoading}
+          />
+      {/* <NotificationsTable
         data={data ? data.Notifications : null}
         isLoading={isLoading}
         btnLabel="Compose"
         btnClick={onOpen}
         tableName="Notifications"
-      />
+      /> */}
       {isOpen && (
         <Form
           onClose={onClose}
