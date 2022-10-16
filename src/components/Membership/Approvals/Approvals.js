095f025d179f5bf438a608b61982ace6aa822b01
Muhumuza Fortune <fortunemuhumuza@gmail.com>
2022-10-16 22:20:21 +0300

    fix: add pagination to approval table

--- src/components/Membership/Approvals/Approvals.js
+++ src/components/Membership/Approvals/Approvals.js
@@ -3,19 +3,33 @@ import NewRequestsTable from "./ApprovalTable/NewRequestsTable";
 import SectionHeader from "../../common/SectionHeader";
 import { useApprovals } from "../../../hooks/useApprovals";
 import SubHeading from "./../../Tasks/SubHeading/SubHeading";
+import Table from "../../common/TableComponent/Table";
+import { procurementApprovalTableColumns } from "../../../lib/tableColumns";
+import Loader from "../../common/UI/Loader/Loader";
 
 const Approvals = () => {
   const { data, isLoading } = useApprovals();
   return (
     <div>
       <SectionHeader title="Approvals" />
-        <NewRequestsTable
+      {isLoading ? (
+        <Loader />
+      ) : (
+      <Table
+          isLoading={isLoading}
+          data={data?data?.approvals : null}
+          showBtn={false}
+          tableName="Fida Assets"
+          columns={ procurementApprovalTableColumns}
+        />
+        /* <NewRequestsTable
           data={data ? data.approvals : null}
           isLoading={isLoading}
           showBtn={false}
           subHeading={<SubHeading title="New Request" />}
           tableName="New approval requests"
-        />
+        /> */
+      )}
     </div>
   );
 };
