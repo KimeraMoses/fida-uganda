ceed8a88e25ce7120ed42c3b785769f1b2388156
KimeraMoses <kimeramoses001@gmail.com>
2022-10-25 22:42:11 +0300

    fix: cleaned unused variables

--- src/components/HumanResource/Approvals/Approvals.js
+++ src/components/HumanResource/Approvals/Approvals.js
@@ -1,62 +1,62 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import SectionHeader from "../../common/SectionHeader";
 // import HRApprovedTable from "./ApprovalTable/ApprovedTable";
-import {useApprovals} from "../../../hooks/useApprovals";
+import { useApprovals } from "../../../hooks/useApprovals";
 import Table from "../../common/TableComponent/Table";
-import {allApprovalsTableColumns} from "../../../lib/tableColumns";
+import { allApprovalsTableColumns } from "../../../lib/tableColumns";
 
 const HRApprovals = () => {
+  const { data: approvals, isLoading } = useApprovals();
+  // console.log(approvals)
 
-    const {data: approvals, isLoading} = useApprovals();
-    // console.log(approvals)
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    setData([]);
+    if (approvals?.approvals?.length) {
+      const dataToSet = approvals?.approvals?.map((b) => {
+        return {
+          ...b,
+          key: b?.id,
+          full_name: b?.user?.full_name
+            ? b.user.full_name
+            : b?.registeredBy?.full_name
+            ? b.registeredBy.full_name
+            : b?.createdBy?.full_name
+            ? b.createdBy.full_name
+            : "N/A",
+          designation: b?.user?.designation
+            ? b.user.designation
+            : b?.createdBy?.designation
+            ? b.createdBy.designation
+            : "N/A",
+        };
+      });
+      setData(dataToSet);
+    }
 
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        setData([]);
-        if (approvals?.approvals?.length) {
-            const dataToSet = approvals?.approvals?.map((b) => {
-                return {
-                    ...b,
-                    key: b?.id,
-                    full_name: b?.user?.full_name ? b.user.full_name
-                        : b?.registeredBy?.full_name
-                            ? b.registeredBy.full_name
-                            :b?.createdBy?.full_name
-                              ? b.createdBy.full_name
-                            : "N/A",
-                    designation: b?.user?.designation ? b.user.designation
-                        : b?.createdBy?.designation
-                    ?b.createdBy.designation
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [approvals]);
 
-                        :"N/A"
-                };
-            });
-            setData(dataToSet);
-        }
-    }, [approvals]);
-
-
-    return (
-        <div>
-            <SectionHeader title="Approvals"/>
-            <Table
-                data={data}
-                columns={allApprovalsTableColumns}
-                loading={isLoading}
-                showBtn={false}
-                hideActions={true}
-                // showActions={true}
-                // onEditHandler={onEditHandler}
-
-            />
-            {/*<HRApprovedTable*/}
-            {/*  isLoading={isLoading}*/}
-            {/*   data={data ? data.approvals : null}*/}
-            {/*  showBtn={false}*/}
-            {/*  tableName={'Human resource approvals'}*/}
-            {/*/>*/}
-        </div>
-    );
+  return (
+    <div>
+      <SectionHeader title="Approvals" />
+      <Table
+        data={data}
+        columns={allApprovalsTableColumns}
+        loading={isLoading}
+        showBtn={false}
+        hideActions={true}
+        // showActions={true}
+        // onEditHandler={onEditHandler}
+      />
+      {/*<HRApprovedTable*/}
+      {/*  isLoading={isLoading}*/}
+      {/*   data={data ? data.approvals : null}*/}
+      {/*  showBtn={false}*/}
+      {/*  tableName={'Human resource approvals'}*/}
+      {/*/>*/}
+    </div>
+  );
 };
 
 export default HRApprovals;
